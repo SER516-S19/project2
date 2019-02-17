79fb9d5cc7aa5430434227b8ed50862868e47cc3
kumarkalyan93 <kkalyan3@asu.edu>
2019-02-16 20:46:45 -0700

    Adding boiler-plate code for Backend - SpringBoot

--- Team_74/Back-End/Quizzer/src/main/java/com/Quizzer/code/dao/UserRepo.java
+++ Team_74/Back-End/Quizzer/src/main/java/com/Quizzer/code/dao/UserRepo.java
@@ -1,7 +1,6 @@
 package com.Quizzer.code.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import com.Quizzer.code.model.db.User;
 
 /**
@@ -10,6 +9,8 @@ import com.Quizzer.code.model.db.User;
  * @author Kumar Prabhu Kalyan
  *
  */
+import com.Quizzer.code.model.User;
+
 public interface UserRepo extends MongoRepository<User, String> {
 
 	public User findByUserName(String userName);
