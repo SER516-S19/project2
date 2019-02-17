c6b06eea1de449ac83a39e1dfa1e92e2f7edd993
Shivam Verma <shivamv2392@gmail.com>
2019-02-17 16:21:03 -0700

    Intial Folder Structure

---
+++ Team_58/src/edu/asupoly/ser516/controller/ProfessorHome.java
@@ -0,0 +1,16 @@
+package edu.asupoly.ser516.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProfessorHome extends HttpServlet  {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+}
