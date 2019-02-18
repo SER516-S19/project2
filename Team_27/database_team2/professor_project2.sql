bb4f0ee143aa849ec590f838c9c07a8027939ba2
raghava  kannikanti <raghavakannikanti@raghavas-MacBook-Air.local>
2019-02-18 15:08:42 -0700

    Added Database script file for Professor

---
+++ Team_27/database_team2/professor_project2.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS `project2`;
+
+USE `project2`;
+
+CREATE TABLE `Quiz`
+(
+	`quiz_id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(255),
+	`instructions` TEXT,
+	`assignment_group` VARCHAR(255),
+	`isShuffled` BOOLEAN,
+	`isGraded` BOOLEAN,
+	`time_limit` INT,
+	`isMultipleAttempt` BOOLEAN,
+    PRIMARY KEY (quiz_id)
+) ;
+
+CREATE TABLE `Questions`
+(
+	`question_id` INT NOT NULL AUTO_INCREMENT,
+    `quiz_id` INT NOT NULL,
+	`question` TEXT,
+	`option1` VARCHAR(1024),
+	`isOption1Correct` BOOLEAN,
+	`option2` varchar(1024),
+	`isOption2Correct` BOOLEAN,
+	`option3` VARCHAR(1024),
+	`isOption3Correct` boolean,
+	`option4` VARCHAR(1024),
+	`isOption4Correct` BOOLEAN,
+	`isMultipleAnswer` BOOLEAN,
+    `points` INT,
+ 	PRIMARY KEY (question_id),
+    FOREIGN KEY (quiz_id) REFERENCES `Quiz`(quiz_id) ON DELETE CASCADE
+);
