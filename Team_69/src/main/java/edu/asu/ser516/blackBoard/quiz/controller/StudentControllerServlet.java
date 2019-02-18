3eaf7984260c9b749b651732d3f60aa83315f83d
sidd <sourabh@aicumen.com>
2019-02-18 06:56:17 -0700

    Add structure for servlet Action on quiz submission

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/controller/StudentControllerServlet.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/controller/StudentControllerServlet.java
@@ -1,4 +1,31 @@
-package main.java.edu.asu.ser516.blackBoard.quiz.controller;
+package edu.asu.ser516.blackBoard.quiz.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class StudentControllerServlet {
+
+	private final static String SUBMIT_ACTION = "submit";
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		String studentId = (String) session.getAttribute("user_id");
+		String action = request.getParameter("action");
+
+		//Servlet Action on quiz submission
+		if(action.equals(SUBMIT_ACTION)) {
+			try {
+				response.sendRedirect("Success.jsp");
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
 }
