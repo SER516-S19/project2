e5f2cc6108a01f554677f072c558fd9be122eaa0
Jinal Patel <43081263+jspatel5@users.noreply.github.com>
2019-02-17 17:54:58 -0700

    Set user_id to auto increment in database

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/bean/User.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/bean/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private int user_id;
 
