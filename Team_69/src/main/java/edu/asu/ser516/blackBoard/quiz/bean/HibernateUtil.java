52a8d7f7e94ed61dfb133e3060cbcfc40ce0112f
Janice Abraham <janicejabraham@gmail.com>
2019-02-18 20:00:37 -0700

    Professor Details submit action

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/bean/HibernateUtil.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/bean/HibernateUtil.java
@@ -19,8 +19,8 @@ public class HibernateUtil {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/quizdb");
-                settings.put(Environment.USER, "user");
-                settings.put(Environment.PASS, "password");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "Janice@2810");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
