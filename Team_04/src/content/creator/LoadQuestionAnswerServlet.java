6b5a5e5cce33396044cbdac30a0d1d0bc393e7f7
Yuvan <psm-asu3@ghsc-psm.org>
2019-02-19 01:56:50 -0700

    #US 06 #Task 22 Base code for rendering question and options

--- Team_04/src/content/creator/LoadQuestionAnswerServlet.java
+++ Team_04/src/content/creator/LoadQuestionAnswerServlet.java
@@ -11,14 +11,19 @@ import javax.servlet.http.HttpSession;
 public class LoadQuestionAnswerServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
+		HttpSession session = request.getSession(true);
 		response.setContentType("text/html");
 		response.setStatus(response.SC_OK);
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
+
+		HttpSession session = request.getSession(true);
+		response.setContentType("text/html");
+		response.setStatus(response.SC_OK);
+		ArrayList<String> quesDesr = new ArrayList<String>();
+
     }
 
 }
