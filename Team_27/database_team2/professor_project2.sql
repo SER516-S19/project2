2ce2120d0bc3838442c01ad6e1b3b89894b16c0b
raghava  kannikanti <raghavakannikanti@Lakshmi-MacBook-Air.local>
2019-02-18 18:33:56 -0700

    Format issue fixed for database files.

--- Team_27/database_team2/professor_project2.sql
+++ Team_27/database_team2/professor_project2.sql
@@ -2,34 +2,34 @@ CREATE DATABASE IF NOT EXISTS `project2_team27`;
 
 USE `project2_team27`;
 
-CREATE TABLE `Quiz`
-(
-	`quiz_id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(255),
-	`instructions` TEXT,
-	`assignment_group` VARCHAR(255),
-	`isShuffled` BOOLEAN,
-	`isGraded` BOOLEAN,
-	`time_limit` INT,
-	`isMultipleAttempt` BOOLEAN,
-    PRIMARY KEY (quiz_id)
-);
+CREATE TABLE `quiz`
+  (
+     `quiz_id`           INT NOT NULL auto_increment,
+     `title`             VARCHAR(255),
+     `instructions`      TEXT,
+     `assignment_group`  VARCHAR(255),
+     `isshuffled`        BOOLEAN,
+     `isgraded`          BOOLEAN,
+     `time_limit`        INT,
+     `ismultipleattempt` BOOLEAN,
+     PRIMARY KEY (quiz_id)
+  );
 
-CREATE TABLE `Questions`
-(
-	`question_id` INT NOT NULL AUTO_INCREMENT,
-    `quiz_id` INT NOT NULL,
-	`question` TEXT,
-	`option1` VARCHAR(1024),
-	`isOption1Correct` BOOLEAN,
-	`option2` varchar(1024),
-	`isOption2Correct` BOOLEAN,
-	`option3` VARCHAR(1024),
-	`isOption3Correct` boolean,
-	`option4` VARCHAR(1024),
-	`isOption4Correct` BOOLEAN,
-	`isMultipleAnswer` BOOLEAN,
-    `points` INT,
- 	PRIMARY KEY (question_id),
-    FOREIGN KEY (quiz_id) REFERENCES `Quiz`(quiz_id) ON DELETE CASCADE
-);
+CREATE TABLE `questions`
+  (
+     `question_id`      INT NOT NULL auto_increment,
+     `quiz_id`          INT NOT NULL,
+     `question`         TEXT,
+     `option1`          VARCHAR(1024),
+     `isoption1correct` BOOLEAN,
+     `option2`          VARCHAR(1024),
+     `isoption2correct` BOOLEAN,
+     `option3`          VARCHAR(1024),
+     `isoption3correct` BOOLEAN,
+     `option4`          VARCHAR(1024),
+     `isoption4correct` BOOLEAN,
+     `ismultipleanswer` BOOLEAN,
+     `points`           INT,
+     PRIMARY KEY (question_id),
+     FOREIGN KEY (quiz_id) REFERENCES `quiz`(quiz_id) ON DELETE CASCADE
+  );
