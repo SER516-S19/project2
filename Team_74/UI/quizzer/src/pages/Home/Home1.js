d014c69c3112252fc967b7d20a9b53600564023e
mspranav <smandad1@asu.edu>
2019-02-20 14:33:55 -0700

    Dev Home Page

---
+++ Team_74/UI/quizzer/src/pages/Home/Home1.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Home.css';
+import {NavLink} from "react-router-dom";
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div className="Home">
+            <center>
+                <h3>Student Portal</h3>
+                <h5>Click on the Quiz to be completed</h5>
+                <br></br></center>
+
+                <ListGroup>
+
+                    <ListGroupItem><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 1</NavLink></ListGroupItem>
+                    <ListGroupItem><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 2</NavLink></ListGroupItem>
+                    <ListGroupItem><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 3</NavLink></ListGroupItem>
+                </ListGroup>
+
+            </div>
+        );
+    }
+}
+
+export default Home;
