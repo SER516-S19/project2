4ed101d9d5c308767f3c56210cdcc8d93fa975c2
Yuvan <psm-asu3@ghsc-psm.org>
2019-02-19 19:49:01 -0700

    #US 06 #Task 22 Added getter and setter

--- Team_04/src/com/model/Answers.java
+++ Team_04/src/com/model/Answers.java
@@ -2,5 +2,41 @@ package com.model;
 
 public class Answers {
 
+    private int ans_id;
+    private String ans_desc;
+    private boolean is_correct;
+    private float max_score;
+
+    public int getAns_id() {
+        return ans_id;
+    }
+
+    public void setAns_id(int ans_id) {
+        this.ans_id = ans_id;
+    }
+
+    public String getAns_desc() {
+        return ans_desc;
+    }
+
+    public void setAns_desc(String ans_desc) {
+        this.ans_desc = ans_desc;
+    }
+
+    public boolean isIs_correct() {
+        return is_correct;
+    }
+
+    public void setIs_correct(boolean is_correct) {
+        this.is_correct = is_correct;
+    }
+
+    public float getMax_score() {
+        return max_score;
+    }
+
+    public void setMax_score(float max_score) {
+        this.max_score = max_score;
+    }
 
 }
