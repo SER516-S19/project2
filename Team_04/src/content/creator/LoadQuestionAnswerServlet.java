086f6e522ba89eb5a270c6bed65ff910119276d8
Yuvan <psm-asu3@ghsc-psm.org>
2019-02-20 17:22:23 -0700

    #US 06 Task #22 Model layer for loading question and answers created

--- Team_04/src/content/creator/LoadQuestionAnswerServlet.java
+++ Team_04/src/content/creator/LoadQuestionAnswerServlet.java
@@ -1,14 +1,19 @@
 package content.creator;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import com.dao.QuestionAnswerDAO;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+@WebServlet("/servlet")
 public class LoadQuestionAnswerServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,8 +28,18 @@ public class LoadQuestionAnswerServlet extends HttpServlet {
 		HttpSession session = request.getSession(true);
 		response.setContentType("text/html");
 		response.setStatus(response.SC_OK);
-		ArrayList<String> quesDesr = new ArrayList<String>();
 
-    }
+		QuestionAnswerDAO qaObj = new QuestionAnswerDAO();
+
+		request.setAttribute("data", qaObj.getResult());
+
+		RequestDispatcher rd =
+				request.getRequestDispatcher("questionsanswers.jsp");
+
+		rd.forward(request, response);
+
+
+
+	}
 
 }
