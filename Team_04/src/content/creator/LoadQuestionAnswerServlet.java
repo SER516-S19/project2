61c1b19956e6984980561b4db381ba18b8ac568f
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2019-02-21 14:00:33 -0700

    Issue [x] Fixed build issues

--- Team_04/src/content/creator/LoadQuestionAnswerServlet.java
+++ Team_04/src/content/creator/LoadQuestionAnswerServlet.java
@@ -3,6 +3,7 @@ package content.creator;
 import com.dao.QuestionAnswerDAO;
 
 import java.io.IOException;
+import java.sql.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,11 +18,43 @@ import javax.servlet.http.HttpSession;
 public class LoadQuestionAnswerServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		response.setContentType("text/html");
-		response.setStatus(response.SC_OK);
 
-    }
+
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+			Connection conn = null;
+			try {
+				// db parameters
+				String url = "jdbc:sqlite:/Users/aj/Developer/SER-516/project2/Team_04/resources/quizDatabase.db";
+
+				// create a connection to the database
+				conn = DriverManager.getConnection(url);
+
+				System.out.println("Connection to SQLite has been established.");
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+					if (conn != null) {
+						try {
+							Statement s =conn.createStatement();
+							ResultSet rs = s.executeQuery("SELECT * FROM quiz_content");
+
+							while (rs.next()) {
+								System.out.println(rs.getString("ques_desc"));
+							}
+
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+
+			}
+	}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
