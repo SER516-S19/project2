be14f4893174da1b9e270fc3218eeb30e7b4d920
Lehar Bhatt <bhattlehar@gmail.com>
2019-02-21 15:49:04 -0700

    Add Comments

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/services/QuestionAnswerGenerator.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/services/QuestionAnswerGenerator.java
@@ -9,14 +9,23 @@ import edu.asu.ser516.blackBoard.quiz.bean.Question;
 import edu.asu.ser516.blackBoard.quiz.bean.QuestionAnswer;
 import edu.asu.ser516.blackBoard.quiz.dao.QuestionDAO;
 
+/**
+ * A class to combine the questions with its answers
+ * 
+ * @author: leharbhatt
+ * @version: 1.0
+ * @since : 02/20/2019
+ *
+ */
+
 public class QuestionAnswerGenerator {
-	public List<QuestionAnswer> generator(int quizId){
+	public List<QuestionAnswer> generator(int quizId) {
 		QuestionDAO questionDao = new QuestionDAO();
 		List<Question> questionList = questionDao.getQuestionsByQuizId(quizId);
 		List<QuestionAnswer> questionAnswerList = new ArrayList<QuestionAnswer>();
-		for(Question question: questionList) {
+		for (Question question : questionList) {
 			question.getQuestionId();
-			//get answer
+			// get answer
 
 			QuestionAnswer questionAnswer = new QuestionAnswer();
 			questionAnswer.setQuestionId(question.getQuestionId());
@@ -24,14 +33,14 @@ public class QuestionAnswerGenerator {
 			questionAnswer.setQuestion(question.getQuestion());
 			questionAnswer.setPoints(question.getPoints());
 			questionAnswer.setMultiple(question.isMultiple());
-			//questionAnswer.setAnswerList("");
+			// questionAnswer.setAnswerList(question.);
 			questionAnswerList.add(questionAnswer);
-			
+
 		}
 		return questionAnswerList;
-		
+
 	}
-	
+
 	public String ObjectToJSON(List<QuestionAnswer> list) {
 		Gson gson = new Gson();
 		String result = gson.toJson(list);
