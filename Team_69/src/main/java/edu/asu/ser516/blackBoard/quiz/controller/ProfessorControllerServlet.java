8cd493d99ed67082307ffe10282aebcf77411b91
Gangadhara Matti <gangadharmatti.16@gmail.com>
2019-02-20 22:34:21 -0700

    Prof US#13 Task#1 - added Code to take input from  Professor and store it to Question table

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/controller/ProfessorControllerServlet.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/controller/ProfessorControllerServlet.java
@@ -1,7 +1,6 @@
 package edu.asu.ser516.blackBoard.quiz.controller;
 import java.io.IOException;
-
-
+import java.sql.Time;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,7 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 
 
 import edu.asu.ser516.blackBoard.quiz.bean.Professor;
+import edu.asu.ser516.blackBoard.quiz.bean.Question;
+import edu.asu.ser516.blackBoard.quiz.bean.Quiz;
 import edu.asu.ser516.blackBoard.quiz.dao.ProfessorDAO;
+import edu.asu.ser516.blackBoard.quiz.dao.QuestionDAO;
 
 
 public class ProfessorControllerServlet extends HttpServlet{
@@ -55,7 +57,29 @@ public class ProfessorControllerServlet extends HttpServlet{
 			//System.out.println("Quiz : "+proffessorDAO.InsertProfDetails());
 		
             response.sendRedirect("views/professorDetails.jsp");
-        }  
+            
+        }else if("addQuestion".equals(flag)) {
+        	String question = request.getParameter("question");
+        	String option1 = request.getParameter("option1");
+        	String option2 = request.getParameter("option2");
+        	String option3 = request.getParameter("option3");
+        	String option4 = request.getParameter("option4");
+        	
+        	Time t = new Time(343443);
+        	Quiz quiz = new Quiz("Quiz1","Hello", "Graded",t, "Y" );
+        	Question q = new Question(quiz, question, 2, 'b',25);
+        	QuestionDAO questionDAO = new QuestionDAO();
+        	questionDAO.addQuestion(q);
+        	System.out.println("Here I AM");
+        	
+        	String contextURL = request.getContextPath();
+        	String addQuestionPageURL = "./ProfessorController";
+        	request.setAttribute("profnavigate", addQuestionPageURL); 
+        	request.getRequestDispatcher("views/AddQuestions.jsp").forward(request, response);
+        	return;
+        }
+		
+		
 
     }
 
