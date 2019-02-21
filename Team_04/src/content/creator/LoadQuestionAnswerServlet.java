e0876d114def30cd2b7a01477d44bb881dd8a5ff
ankitaShivanand <ankitashivanand25@gmail.com>
2019-02-20 23:15:43 -0700

    US06#Task21 Code for connection

--- Team_04/src/content/creator/LoadQuestionAnswerServlet.java
+++ Team_04/src/content/creator/LoadQuestionAnswerServlet.java
@@ -26,6 +26,7 @@ public class LoadQuestionAnswerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession(true);
+
 		response.setContentType("text/html");
 		response.setStatus(response.SC_OK);
 
