f85bbb8f59dadedbd1d67d3495b4af092aba7a35
bpanda1 <bpanda1@asu.edu>
2019-02-21 15:33:33 -0700

    Added Dao-bean layer for add question feature

---
+++ Team_27/SER516_Quiz/src/edu/asu/ser516/quiz/dao/impl/QuestionsDaoBean.java
@@ -0,0 +1,18 @@
+package edu.asu.ser516.quiz.dao.impl;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class QuestionsDaoBean {
+
+	public QuestionsDaoBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
