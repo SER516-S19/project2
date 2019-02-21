922733ead94e3eead2f0123fa771fe5e10597a5e
bpanda1 <bpanda1@asu.edu>
2019-02-20 18:41:17 -0700

    Us08-Task36: Created Servlet to read all the parameters from HTML

--- Team_27/addQuestionsServlet.java
+++ Team_27/addQuestionsServlet.java
@@ -7,43 +7,60 @@ import javax.servlet.http.*;
 public class addQuestionsServlet extends HttpServlet {
 
 	// Method to handle GET method request.
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-	   throws ServletException, IOException {
-	   
-	   // Set response content type
-	   response.setContentType("text/html");
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+	    
+	    // Set response content type
+	    response.setContentType("text/html");
 	
-	   PrintWriter out = response.getWriter();
-	   String title = "Using GET Method to Read Form Data";
-	   String docType =
-	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
-	      "transitional//en\">\n";
-	      
-	   out.println(docType +
-	      "<html>\n" +
-	         "<head><title>" + title + "</title></head>\n" +
-	         "<body bgcolor = \"#f0f0f0\">\n" +
-	            "<h1 align = \"center\">" + title + "</h1>\n" +
-	            "<ul>\n" +
-	               "  <li><b>Question 1</b>: "
-	               + request.getParameter("question1") + "\n" +
-	               "  <li><b>Option A</b>: "
-	               + request.getParameter("OptionAForQues1") + "\n" +
-	               "  <li><b>Option B</b>: "
-	               + request.getParameter("OptionBForQues1") + "\n" +
-	               "  <li><b>Option C</b>: "
-	               + request.getParameter("OptionCForQues1") + "\n" +
-	               "  <li><b>Option D</b>: "
-	               + request.getParameter("OptionDForQues1") + "\n" +
-	            "</ul>\n" +
-	         "</body>" +
-	      "</html>"
-	   );
-	}
+	    PrintWriter out = response.getWriter();
+	    String title = "Reading All Form Parameters";
+	    String docType =
+	       "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
 	
-	// Method to handle POST method request.
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-	   throws ServletException, IOException {
-		doGet(request, response);
-	}
+	    out.println(docType +
+	       "<html>\n" +
+	       "<head><title>" + title + "</title></head>\n" +
+	       "<body bgcolor = \"#f0f0f0\">\n" +
+	       "<h1 align = \"center\">" + title + "</h1>\n" +
+	       "<table width = \"100%\" border = \"1\" align = \"center\">\n" +
+	       "<tr bgcolor = \"#949494\">\n" +
+	          "<th>Param Name</th>" +
+	          "<th>Param Value(s)</th>\n"+
+	       "</tr>\n"
+	    );
+	
+	    Enumeration paramNames = request.getParameterNames();
+	
+	    while(paramNames.hasMoreElements()) {
+	       String paramName = (String)paramNames.nextElement();
+	       out.print("<tr><td>" + paramName + "</td>\n<td>");
+	       String[] paramValues = request.getParameterValues(paramName);
+	
+	       // Read single valued data
+	       if (paramValues.length == 1) {
+	          String paramValue = paramValues[0];
+	          if (paramValue.length() == 0)
+	             out.println("<i>No Value</i>");
+	             else
+	             out.println(paramValue);
+	       } else {
+	          // Read multiple valued data
+	          out.println("<ul>");
+	
+	          for(int i = 0; i < paramValues.length; i++) {
+	             out.println("<li>" + paramValues[i]);
+	          }
+	          out.println("</ul>");
+	       }
+	    }
+	    out.println("</tr>\n</table>\n</body></html>");
+    }
+ 
+	 // Method to handle POST method request.
+	 public void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	    
+	    doGet(request, response);
+	 }
 }
