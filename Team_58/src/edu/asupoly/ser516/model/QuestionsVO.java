dc2e879cbdcc84912c87b904007c271ed6d4c124
Trupti Khatavkar <khatavkar.trupti@gmail.com>
2019-02-22 16:10:26 -0700

    Task: code refactoring and styling of create quesions page

--- Team_58/src/edu/asupoly/ser516/model/QuestionsVO.java
+++ Team_58/src/edu/asupoly/ser516/model/QuestionsVO.java
@@ -9,8 +9,8 @@ package edu.asupoly.ser516.model;
  */
 
 public class QuestionsVO {
-	public QuestionsVO(int qId, String question, String correctAnswer, String incorrectAnswer1, String incorrectAnswer2,
-			String incorrectAnswer3, int totalPoints) {
+	public QuestionsVO(int qId, int totalPoints, String correctAnswer, String incorrectAnswer1, String incorrectAnswer2,
+			String incorrectAnswer3, String question) {
 		super();
 		this.qId = qId;
 		this.question = question;
