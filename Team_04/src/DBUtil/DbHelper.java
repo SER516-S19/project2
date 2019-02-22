4cb237b26822e41a0fd236e5c1bf31bb5499c0da
ankitaShivanand <ankitashivanand25@gmail.com>
2019-02-22 01:15:40 -0700

    #US06#Task21 Code for externalizing DB and fetching from property file

--- Team_04/src/DBUtil/DbHelper.java
+++ Team_04/src/DBUtil/DbHelper.java
@@ -1,9 +1,23 @@
 package DBUtil;
 
+import java.util.Properties;
+
 public class DbHelper {
-    private static final String DB_URL =
-            "jdbc:sqlite:C:/Users/14807/Documents/Project516/project2/Team_04/src/quizDatabase.db";
+    private static String DB_URL ;
     public static String getDbUrl() {
+        System.out.println("DB_URL"+DB_URL);
         return DB_URL;
+    }
+
+
+    static {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.load(DbHelper.class.getClassLoader().getResourceAsStream("DBDetails.properties"));
+            DB_URL    = dbProperties.getProperty("jdbcUrl");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+        }
     }
 }
