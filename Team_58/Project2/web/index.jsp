59fd48cf3d65b317ba6dd42999c244c3f3c18671
Josh <jkdrumm1@gmail.com>
2019-02-22 02:01:31 -0700

    Changed project structure a bit. Will consider reworking further. Edited the StudentHomeServlet to begin to test the page.

---
+++ Team_58/Project2/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Feb 22, 2019, 12:31:21 AM
+    Author     : Joshua
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
--- Team_58/src/edu/asupoly/ser516/controller/StudentHomeServlet.java
+++ Team_58/src/edu/asupoly/ser516/controller/StudentHomeServlet.java
@@ -2,6 +2,7 @@ package edu.asupoly.ser516.controller;
 
 import edu.asupoly.ser516.model.CourseVO;
 import edu.asupoly.ser516.model.UserVO;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,6 +10,8 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,9 +19,74 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet(name = "FormServlet", urlPatterns = "/studentWelcome")
-public class StudentWelcomeServlet extends HttpServlet
+@WebServlet(name = "StudentHome", urlPatterns = "/StudentHome")
+public class StudentHomeServlet extends HttpServlet
 {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+    {
+        try
+        {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String hostName = "showtimefinder.database.windows.net"; // update me
+            String dbName = "ser516_db"; // update me
+            String user = "scrum_mates@showtimefinder"; // update me
+            String password = "Azure@Cloud"; // update me
+            String url = String.format(
+                "jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
+                hostName, dbName, user, password);
+            Connection connection = null;
+            connection = DriverManager.getConnection(url);
+
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            PreparedStatement query;
+            query = connection.prepareStatement("select * from [dbo].[Course] A " +
+                                                " join [dbo].[UserCourseMApping] B" +
+                                                " on A.courseId = B.courseId" + " where B.userId = ?");
+            query.setInt(1, 8);//student.getUserId());
+
+            ResultSet resultData = query.executeQuery();
+            
+            PreparedStatement query2;
+            query2 = connection.prepareStatement("select * from [dbo].[UserDetails] " +
+                                                "where B.userId = ?");
+            query2.setInt(1, 8);
+
+            ResultSet userData = query2.executeQuery();
+            
+            System.out.println(userData);
+
+            List<CourseVO> list = new ArrayList<>();
+
+            while (resultData.next()) {
+                    int courseId = resultData.getInt("courseId");
+                    String courseName = resultData.getString("courseName");
+                    String courseNumber = resultData.getString("courseNumber");
+                    CourseVO course = new CourseVO(courseName, courseNumber, courseId);
+                    list.add(course);
+            }
+
+            HashMap<Integer, String> course = new HashMap<>();
+            for (int i = 0; i < list.size(); i++)
+                    course.put(list.get(i).getCourseId(), list.get(i).getCourseName());
+
+            //session.setAttribute("CourseHashMap", course);
+
+            System.out.println(req.getContextPath() + "/studentHome.ftl");
+
+            res.sendRedirect(req.getContextPath() + "/studentHome.ftl");
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse res)
     {
@@ -69,7 +137,7 @@ public class StudentWelcomeServlet extends HttpServlet
 
             session.setAttribute("CourseHashMap", course);
 
-            System.out.println(req.getContextPath() + "/studnetHome.ftl");
+            System.out.println(req.getContextPath() + "/studentHome.ftl");
 
             res.sendRedirect(req.getContextPath() + "/studentHome.ftl");
             
@@ -79,4 +147,3 @@ public class StudentWelcomeServlet extends HttpServlet
         }
     }
 }
-
