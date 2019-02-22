08557fa9904f4a5229145f09c7e74468e54d919f
aneeshdalvi <adalvi1@asu.edu>
2019-02-21 20:47:15 -0700

    US#10 Task 1-3 Fetching Questions and Answer Details to UI

--- Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/dao/QuestionDAO.java
+++ Team_69/src/main/java/edu/asu/ser516/blackBoard/quiz/dao/QuestionDAO.java
@@ -2,9 +2,13 @@ package edu.asu.ser516.blackBoard.quiz.dao;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.criteria.*;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+
+import edu.asu.ser516.blackBoard.quiz.bean.Answer;
 import edu.asu.ser516.blackBoard.quiz.bean.HibernateUtil;
 import edu.asu.ser516.blackBoard.quiz.bean.Question;
 import edu.asu.ser516.blackBoard.quiz.bean.Quiz;
@@ -97,6 +101,36 @@ public class QuestionDAO {
 			return ;
 		}
 		return ;
+	}
+	public List<Answer> getQuestionsAndAnswers(int quizId) {
+		// TODO Auto-generated method stub
+	
+		Transaction transaction = null;
+	       List<Answer> quesList = new ArrayList<Answer>();
+	       try  {
+	           Session session = HibernateUtil.getSessionFactory().openSession();
+	           transaction = session.beginTransaction();
+	           CriteriaBuilder builder = session.getCriteriaBuilder();
+	           CriteriaQuery<Answer> query = builder.createQuery(Answer.class);
+	           Root<Answer> root = query.from(Answer.class);
+	           Join<Answer,Question> join = root.join("question");
+	           query.select(root).where(root.get("quiz").in(quizId));
+	           Query<Answer> q=session.createQuery(query);
+	           quesList= q.getResultList();
+	           for (Answer name : quesList) {
+	               System.out.println(name);
+	           }
+	           transaction.commit();
+	       } catch (HibernateException e) {
+	           e.printStackTrace();
+	           if (transaction != null) {
+	               transaction.rollback();
+	           }
+	       }
+	       return quesList;
+
+		
+		
 	}
 	
 }
