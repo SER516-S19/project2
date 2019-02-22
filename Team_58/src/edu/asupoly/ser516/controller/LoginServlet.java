2b082b7a4c20373cb1686ffdc635b2cf36ec77ca
Aditya Vikram <adityavikram@adityas-mbp.3500.dhcp.asu.edu>
2019-02-22 01:27:14 -0700

    Minor bug fixes in build files

--- Team_58/src/edu/asupoly/ser516/controller/LoginServlet.java
+++ Team_58/src/edu/asupoly/ser516/controller/LoginServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import edu.asupoly.ser516.model.UserDAOBean;
 import edu.asupoly.ser516.model.UserVO;
 
