a86bdaa4a8a71a23d571d943ddddf9fb04f8889c
sidd <sourabh@aicumen.com>
2019-02-21 23:14:57 -0700

    Get quiz data on quiz UI in JSON format

--- Team_69/web/js/student.js
+++ Team_69/web/js/student.js
@@ -4,163 +4,175 @@
  * @since - 02/19/2019
  */
 
-(function() {
-	var questions=[];
+// Displays all questions with answers
+function displayQuiz() {
+	var questions = [{"questionId":3,"quiz":{"quizId":1,"quizName":"Quiz1","quizInstructions":"Read","quizType":"Graded","quizTimeLimit":"12:30:00 AM","isShuffled":true,"isPublished":false},"question":"Question3","correctAnswerId":0,"isMultiple":true,"points":10},{"questionId":4,"quiz":{"quizId":1,"quizName":"Quiz1","quizInstructions":"Read","quizType":"Graded","quizTimeLimit":"12:30:00 AM","isShuffled":true,"isPublished":false},"question":"Question4","correctAnswerId":0,"isMultiple":true,"points":10}];
+	var questionCounter = 0; // Tracks question number
+	var selections = []; // Array containing user choices
+	var quiz = $('#quiz'); // Quiz div object
+
+	// Display initial question
+	displayNext();
+
+	// Click handler for the 'next' button
+	$('#next').on('click', function(e) {
+		e.preventDefault();
+
+		// Suspend click listener during fade animation
+		if (quiz.is(':animated')) {
+			return false;
+		}
+		choose();
+
+		// If no user selection, progress is stopped
+		if (isNaN(selections[questionCounter])) {
+			alert('Please make a selection!');
+		} else {
+			questionCounter++;
+			displayNext();
+		}
+	});
+
+	// Click handler for the 'prev' button
+	$('#prev').on('click', function(e) {
+		e.preventDefault();
+
+		if (quiz.is(':animated')) {
+			return false;
+		}
+		choose();
+		questionCounter--;
+		displayNext();
+	});
+
+	// Click handler for the 'Start Over' button
+	$('#start').on('click', function(e) {
+		e.preventDefault();
+
+		if (quiz.is(':animated')) {
+			return false;
+		}
+		questionCounter = 0;
+		selections = [];
+		displayNext();
+		$('#start').hide();
+	});
+
+	// Animates buttons on hover
+	$('.button').on('mouseenter', function() {
+		$(this).addClass('active');
+	});
+	$('.button').on('mouseleave', function() {
+		$(this).removeClass('active');
+	});
+
+	// Creates and returns the div that contains the questions and
+	// the answer selections
+	function createQuestionElement(index) {
+		var qElement = $('<div>', {
+			id : 'question'
+		});
+
+		var header = $('<h2>Question ' + (index + 1)
+				+ ': <span float:"right"> Points </span></h2>');
+		qElement.append(header);
+
+		var question = $('<p>').append(questions[index].question);
+		qElement.append(question);
+
+		var radioButtons = createRadios(index);
+		qElement.append(radioButtons);
+
+		return qElement;
+	}
+
+	// Creates a list of the answer choices as radio inputs
+	function createRadios(index) {
+		var radioList = $('<ul>');
+		var item;
+		var input = '';
+		for (var i = 0; i < questions[index].choices.length; i++) {
+			item = $('<li>');
+			input = '<input type="radio" name="answer" value=' + i + ' />';
+			input += questions[index].choices[i];
+			item.append(input);
+			radioList.append(item);
+		}
+		return radioList;
+	}
+
+	// Reads the user selection and pushes the value to an array
+	function choose() {
+		selections[questionCounter] = +$('input[name="answer"]:checked').val();
+	}
 	
-	loadDoc();
-	function loadDoc() {
-		  var xhttp = new XMLHttpRequest();
-		  xhttp.onreadystatechange = function() {
-		    if (this.readyState == 4 && this.status == 200) {
-		    	questions=JSON.parse(this.responseText).results;
-		     console.log(questions);
-		     
-		     var questionCounter = 0; //Tracks question number
-			  var selections = []; //Array containing user choices
-			  var quiz = $('#quiz'); //Quiz div object
-			  
-			  // Display initial question
-			  displayNext();
-			  
-			  // Click handler for the 'next' button
-			  $('#next').on('click', function (e) {
-			    e.preventDefault();
-			    
-			    // Suspend click listener during fade animation
-			    if(quiz.is(':animated')) {        
-			      return false;
-			    }
-			    choose();
-			    
-			    // If no user selection, progress is stopped
-			    if (isNaN(selections[questionCounter])) {
-			      alert('Please make a selection!');
-			    } else {
-			      questionCounter++;
-			      displayNext();
-			    }
-			  });
-			  
-			  // Click handler for the 'prev' button
-			  $('#prev').on('click', function (e) {
-			    e.preventDefault();
-			    
-			    if(quiz.is(':animated')) {
-			      return false;
-			    }
-			    choose();
-			    questionCounter--;
-			    displayNext();
-			  });
-			  
-			  // Click handler for the 'Start Over' button
-			  $('#start').on('click', function (e) {
-			    e.preventDefault();
-			    
-			    if(quiz.is(':animated')) {
-			      return false;
-			    }
-			    questionCounter = 0;
-			    selections = [];
-			    displayNext();
-			    $('#start').hide();
-			  });
-			  
-			  // Animates buttons on hover
-			  $('.button').on('mouseenter', function () {
-			    $(this).addClass('active');
-			  });
-			  $('.button').on('mouseleave', function () {
-			    $(this).removeClass('active');
-			  });
-			  
-			  // Creates and returns the div that contains the questions and 
-			  // the answer selections
-			  function createQuestionElement(index) {
-			    var qElement = $('<div>', {
-			      id: 'question'
-			    });
-			    
-			    var header = $('<h2>Question ' + (index + 1) + ': <span float:"right"> Points </span></h2>');
-			    qElement.append(header);
-			    
-			    var question = $('<p>').append(questions[index].question);
-			    qElement.append(question);
-			    
-			    var radioButtons = createRadios(index);
-			    qElement.append(radioButtons);
-			    
-			    return qElement;
-			  }
-			  
-			  // Creates a list of the answer choices as radio inputs
-			  function createRadios(index) {
-			    var radioList = $('<ul>');
-			    var item;
-			    var input = '';
-			    for (var i = 0; i < questions[index].choices.length; i++) {
-			      item = $('<li>');
-			      input = '<input type="radio" name="answer" value=' + i + ' />';
-			      input += questions[index].choices[i];
-			      item.append(input);
-			      radioList.append(item);
-			    }
-			    return radioList;
-			  }
-			  
-			  // Reads the user selection and pushes the value to an array
-			  function choose() {
-			    selections[questionCounter] = +$('input[name="answer"]:checked').val();
-			  }
-			  
-			  // Displays next requested element
-			  function displayNext() {
-			    quiz.fadeOut(function() {
-			      $('#question').remove();
-			      
-			      if(questionCounter < questions.length){
-			        var nextQuestion = createQuestionElement(questionCounter);
-			        quiz.append(nextQuestion).fadeIn();
-			        if (!(isNaN(selections[questionCounter]))) {
-			          $('input[value='+selections[questionCounter]+']').prop('checked', true);
-			        }
-			        
-			        // Controls display of 'prev' button
-			        if(questionCounter === 1){
-			          $('#prev').show();
-			        } else if(questionCounter === 0){
-			          
-			          $('#prev').hide();
-			          $('#next').show();
-			        }
-			      }else {
-			        var scoreElem = displayScore();
-			        quiz.append(scoreElem).fadeIn();
-			        $('#next').hide();
-			        $('#prev').hide();
-			        $('#start').show();
-			      }
-			    });
-			  }
-			  
-			  // Computes score and returns a paragraph element to be displayed
-			  function displayScore() {
-			    var score = $('<p>',{id: 'question'});
-			    
-			    var numCorrect = 0;
-			    for (var i = 0; i < selections.length; i++) {
-			      if (selections[i] === questions[i].correctAnswer) {
-			        numCorrect++;
-			      }
-			    }
-			    
-			    score.append('You got ' + numCorrect + ' questions out of ' +
-			                 questions.length + ' right!!!');
-			    return score;
-			  }		     
-		    }
-		  };
-		  xhttp.open("GET", "student", true);
-		  xhttp.send();
-		}	  
-	})();
+	function autoSave() {
+		console.log("Saved!");
+
+		var questionContainers = $(".question");
+		for (var i = 0; i < questionContainers.length; i++) {
+			var questionID = questionContainers[i].id;
+			var ansElems = questionContainers[i]
+					.getElementsByTagName("input");
+			for (var j = 0; j < ansElems.length; j++) {
+				if (ansElems[j].checked) {
+					console.log(questionID + ": " + ansElems[j].value);
+					//Save in session or temp Table?
+				}
+			}
+		}
+	}
+
+	// Displays next requested element
+	function displayNext() {
+		quiz.fadeOut(function() {
+			$('#question').remove();
+
+			if (questionCounter < questions.length) {
+				var nextQuestion = createQuestionElement(questionCounter);
+				quiz.append(nextQuestion).fadeIn();
+				if (!(isNaN(selections[questionCounter]))) {
+					$('input[value=' + selections[questionCounter] + ']').prop(
+							'checked', true);
+				}
+
+				// Controls display of 'prev' button
+				if (questionCounter === 1) {
+					$('#prev').show();
+				} else if (questionCounter === 0) {
+
+					$('#prev').hide();
+					$('#next').show();
+				}
+			} else {
+				var scoreElem = displayScore();
+				quiz.append(scoreElem).fadeIn();
+				$('#next').hide();
+				$('#prev').hide();
+				$('#start').show();
+			}
+		});
+	}
+	
+	function autoSave() {
+		console.log("Saved!");
+
+		var questionContainers = $(".question");
+		for (var i = 0; i < questionContainers.length; i++) {
+			var questionID = questionContainers[i].id;
+			var ansElems = questionContainers[i]
+					.getElementsByTagName("input");
+			for (var j = 0; j < ansElems.length; j++) {
+				if (ansElems[j].checked) {
+					console.log(questionID + ": " + ansElems[j].value);
+					//Save in session or temp Table?
+				}
+			}
+		}
+	}
+	
+	function submitQuiz() {
+		autoSave();
+		//Send session data to controller
+	}
+}
--- Team_69/web/views/student.jsp
+++ Team_69/web/views/student.jsp
@@ -4,22 +4,15 @@
 <html>
 <head>
 <title>Start your quiz</title>
-	<link type='text/css' rel='stylesheet' href='stylesheet.css' />
-	<link rel="stylesheet" type="text/css"
-		href="https://fonts.googleapis.com/css?family=Open Sans" />
-	<script type='text/javascript'
-		src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
-	<script type="text/javascript" src='questions.json'></script>
-	<script type='text/javascript' src='../js/jsquiz.js'></script>
-
+<link type='text/css' rel='stylesheet' href='../css/studentStyle.css' />
+<link rel="stylesheet" type="text/css"
+	href="https://fonts.googleapis.com/css?family=Open Sans" />
+<script type='text/javascript'
+	src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+<script type='text/javascript' src='../js/jsquiz.js'></script>
+<script type='text/javascript' src='../js/student.js'></script>
 </head>
 <body>
-    <div id = "ridge">
-        <%
-            String quizName = (String) request.getAttribute("QuizName");
-        %>
-    <p><h3><%=quizName%></h3></p>
-    </div>
 	<div id='container'>
 		<div id='title'>
 			<h1>Quiz Instructions</h1>
@@ -39,5 +32,13 @@
     		<button class='' id='prev'>Prev</a></button>
     		<button class='' id='start'> Start Over</a></button> -->
 	</div>
+	<script>
+		$(document).ready(function() {
+			var jsonDataStr = '<%=request.getAttribute("jsonData")%>';
+			var jsonDataObj = JSON.parse(jsonDataStr);
+			displayQuiz(jsonDataObj);
+			var autoSaveInterval = setInterval(autoSave, 2000);
+		});
+	</script>
 </body>
 </html>
