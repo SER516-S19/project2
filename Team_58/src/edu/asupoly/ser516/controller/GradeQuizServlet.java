0b3e83a7658cb6b2388ca625ac2defd0c936129f
AkashKadam75 <kadamakash75@gmail.com>
2019-02-22 11:25:31 -0700

    US#5: Task 2 & Task 3 : Update version 2.0 grade Quiz

--- Team_58/src/edu/asupoly/ser516/controller/GradeQuizServlet.java
+++ Team_58/src/edu/asupoly/ser516/controller/GradeQuizServlet.java
@@ -2,12 +2,10 @@ package edu.asupoly.ser516.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -17,14 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.asupoly.ser516.model.QuizVO;
+import edu.asupoly.ser516.model.GradeQuizVO;
 
 /**
  * Class GradeQuizServlet is a controller 
  * that routes the Professor to Grade Dashboard Page from Professor Home Page.
  * 
  * @author akashkadam
- * @version 1.1
+ * @version 1.2
  **/
 public class GradeQuizServlet extends HttpServlet{
 
@@ -44,18 +42,10 @@ public class GradeQuizServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-
-
 		HttpSession session = request.getSession();
-		HashMap<Integer, String> quizMap = (HashMap<Integer,String>)session.getAttribute("quizMap");
 
-		int quizId = Integer.parseInt(request.getParameter("Quiz"));
-
-		String quizName = quizMap.get(quizId);
-
-		session.setAttribute("quizName",quizName);
-		session.setAttribute("quizId", quizId);
-
+		int quizId = Integer.parseInt(session.getAttribute("quizId").toString());
+		
 
 		try {
 
@@ -75,29 +65,43 @@ public class GradeQuizServlet extends HttpServlet{
 
 
 			PreparedStatement query;
-			query = connection.prepareStatement("select * from [dbo].[StudenrResponse] where quizId = ?");
+			query = connection.prepareStatement("UPDATE SR\r\n" + 
+					"SET SR.[score] = Q.[totalPoints]\r\n" + 
+					"FROM [dbo].[StudentResponse] SR\r\n" + 
+					"JOIN [dbo].[Questions] Q\r\n" + 
+					"ON SR.questionId = Q.questionId\r\n" + 
+					"Where SR.quizId = ?");
 			query.setInt(1,quizId);
 
 			ResultSet resultData = query.executeQuery();
+			
+			query = connection.prepareStatement("Select sum(score) as score, firstname,lastname, C.[quizTitle] \r\n" + 
+					"from StudentResponse A\r\n" + 
+					"JOIN UserDetails B\r\n" + 
+					"on A.userId = B.userId\r\n" + 
+					"JOIN Quiz C\r\n" + 
+					"on C.quizId = A.quizId\r\n" + 
+					"where A.quizId = ?\r\n" + 
+					"group by firstname,lastname, [quizTitle]");
+			query.setInt(1,quizId);
+			
+			resultData = query.executeQuery();
+			
 
-			List<QuizVO> list = new ArrayList<>();
+			List<GradeQuizVO> list = new ArrayList<>();
 
 			while(resultData.next()) {
-				int assignedTime = resultData.getInt("assignedTime");
-				boolean isGraded = resultData.getBoolean("isGraded");
-				String quizInstruction = resultData.getString("quizInstruction"); 
-				Date quizScheduledDate = resultData.getDate("quizScheduledDate");
-				boolean isShuffled = resultData.getBoolean("isShuffled");
-				String quizTitle = resultData.getString("quizTitle");
-				QuizVO quiz = new QuizVO(quizId, isGraded, assignedTime, quizInstruction, quizScheduledDate, isShuffled, quizTitle);
-				list.add(quiz);
+				int score = resultData.getInt("score");
+				String firstName = resultData.getString("firstname");
+				String lastName = resultData.getString("lastname"); 
+				String quizName = resultData.getString("quizName");
+				GradeQuizVO gradeQuiz = new GradeQuizVO(score,firstName,lastName,quizName);
+				list.add(gradeQuiz);
 			}
 
-			HashMap<Integer, String> quiz = new HashMap<>();
-			for(int i=0;i<list.size();i++)
-				quiz.put(list.get(i).getQuizId(), list.get(i).getQuizTitle());
-
-			session.setAttribute("QuizHashMap", quiz);
+			
+			session.setAttribute("gradeQuiz", list);
+			session.setAttribute("quizName", "quiz");
 
 			response.sendRedirect(request.getContextPath()+"/gradeQuiz.ftl");  
 		}
