f7e349eb9c560676fd7c11f5c9296d8be6f9324d
Paul Horton <pahorton@asu.edu>
2019-02-22 01:11:45 -0700

    Enrolled Business object and DAO

---
+++ Team_47/QuizCreator/src/main/java/com/asu/ser516/team47/database/EnrolledDAO.java
@@ -0,0 +1,50 @@
+package com.asu.ser516.team47.database;
+
+import java.util.List;
+
+/**
+ * An interface for a Enrolled Data Access Object
+ * Created by paulhorton on 2/22/19.
+ */
+public interface EnrolledDAO {
+    /**
+     * Gets all the enrollments in the database
+     * @return all enrollments
+     */
+    List<Enrolled> getAllEnrollment();
+
+    /**
+     * Gets all enrollments for a course
+     * @param course_fk the course's id
+     * @return all enrollments in a course
+     */
+    List<Enrolled> getCourseEnrollment(int course_fk);
+
+    /**
+     * Gets all enrollments for a student
+     * @param student_fk the student's username
+     * @return all enrollments in a student
+     */
+    List<Enrolled> getStudentEnrollment(String student_fk);
+
+    /**
+     * Gets a enrollment based on id
+     * @param Enrolled_id a Enrolled id
+     * @return the Enrolled business object
+     */
+    Enrolled getEnrolled(int Enrolled_id);
+
+    /**
+     * Updates a Enrolled in the database based on the
+     * values in a business object
+     * @param enrolled
+     */
+    void updateEnrolled(Enrolled enrolled);
+
+    /**
+     * Deletes a Enrolled in the database based on the
+     * values in a business object.
+     * @param enrolled
+     */
+    void deleteEnrolled(Enrolled enrolled);
+}
