f8b10f98b62fbba62a9f87fcba12a5a28ec600f5
Aditya Samant <adityasamant2505@gmail.com>
2019-02-22 14:54:09 -0700

    UserStory#8Task#9 Added DAO for better abstraction

--- Team_58/src/edu/asupoly/ser516/model/QuestionsVO.java
+++ Team_58/src/edu/asupoly/ser516/model/QuestionsVO.java
@@ -64,7 +64,11 @@ public class QuestionsVO {
 	public void setIncorrectAnswer3(String incorrectAnswer3) {
 		this.incorrectAnswer3 = incorrectAnswer3;
 	}
-
+	
+	
+	public int getQuestionsId() {
+		return qId;
+	}
 	public QuestionsVO(String question, String correctAnswer, String incorrectAnswer1, String incorrectAnswer2,
 			String incorrectAnswer3) {
 		super();
