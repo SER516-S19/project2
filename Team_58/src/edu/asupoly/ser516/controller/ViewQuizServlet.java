61ac11a55e078040ef0ec090196dd2a65b40f39f
Aditya Samant <adityasamant2505@gmail.com>
2019-02-22 14:54:09 -0700

    UserStory#8Task#9 Added DAO for better abstraction

--- Team_58/src/edu/asupoly/ser516/controller/ViewQuizServlet.java
+++ Team_58/src/edu/asupoly/ser516/controller/ViewQuizServlet.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
 
 import edu.asupoly.ser516.model.QuestionsVO;
 import edu.asupoly.ser516.model.QuizVO;
+import edu.asupoly.ser516.model.ViewQuizDAOBean;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -30,10 +31,14 @@ import org.json.simple.parser.JSONParser;
 
 
 /**
- * Servlet code
- * 
+ * Servlet code takes quizId from courseDashboard.ftl  and renders a page displaying
+ * information about the quiz and it's questions.
+ * Database connections and retrievals are all conducted via the DAO interface.
  * @author Aditya Samant
  * @version 1.1
+ * @see resources/courseDashboard.ftl
+ * @see edu.asupoly.ser516.model/ViewQuizDAOBean.java
+ * @see resources/viewQuiz.ftl
  * */
 public class ViewQuizServlet extends HttpServlet{
 	// This servlet will not make any Get requests.
@@ -54,7 +59,7 @@ public class ViewQuizServlet extends HttpServlet{
 	public void doPost(HttpServletRequest req, HttpServletResponse res)  throws ServletException, IOException{
 		   
 	       HttpSession session = req.getSession();
-	       List<QuestionsVO> quizQuestions = new ArrayList<>();
+	       List<QuestionsVO> quizQuestions = new ArrayList<QuestionsVO>();
 	       int quizId = Integer.parseInt(req.getParameter("Quiz"));
 	       
 	       
@@ -68,94 +73,39 @@ public class ViewQuizServlet extends HttpServlet{
 	       Date today = new Date(cal.getTime().getTime());
 	       boolean isAfter = false;
 	       
-	       //Initialize Quiz Information
-	       String quizName = "";
-	       String instruction = "";
-	       Date scheduledDate = new Date(0);
-	       boolean graded = true;
-	       
 	       //counter to addup and get total points for the quiz
 	       int total = 0;
 	       
 	       try {
-	    	   //The information in the following items will be placed securely on a seperate file.
-	    	   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	    	  
-	           String hostName = "showtimefinder.database.windows.net"; 
-	           String dbName = "ser516_db"; 
-	           String user = "scrum_mates@showtimefinder"; 
-	           String password = "Azure@Cloud"; 
-	           String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-	               + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
-	           Connection connection = null;
-	   		   connection = DriverManager.getConnection(url);
-	   		   String schema;
-	   		   schema = connection.getSchema();
-	   		   System.out.println("Successful connection - Schema: " + schema); 
-	   		   
-	   		   //Query strings to gets entire question info and only the isGraded boolean from Quiz table
-	   		   PreparedStatement query;
-	   		   query = connection.prepareStatement("select A.*, B.isGraded, B.quizTitle, B.quizScheduledDate, B.quizInstruction " 
-	   				+ "from [dbo].[Questions] A "
-	   				+ "join [dbo].[Quiz] B "
-	   				+ "on A.quizId = B.quizId "
-	   		   		+ "where A.quizId = ?");
-	   		   query.setInt(1, quizId);
-	   		   ResultSet result = query.executeQuery();
-	   		   
-	   		   while (result.next()) {
-	   			   //Questions DB results 
-	   			   int questionId = result.getInt("questionId");
-	   			   int points = result.getInt("totalPoints");
-	   			   String question = result.getString("question");
-	   			   //Values read as strings but actually JSON
-	   			   String answer = result.getString("actualAnswer"); 
-	   			   String choices = result.getString("totalChoices"); 
-	   			   
-	   			   //Parse Json String objects to json Object
-	   			   JSONParser parser = new JSONParser();
-	   			   JSONObject jo = (JSONObject) parser.parse(choices);
-	   			   JSONObject jo2 = (JSONObject) parser.parse(answer);
-	   			   
-	   			   String choice1 = (String) jo.get("incorrectAnswer1");
-	   			   String choice2 = (String) jo.get("incorrectAnswer2");
-	   			   String choice3 = (String) jo.get("incorrectAnswer3");
-	   			   String ans = (String) jo2.get("CORRECT");
-	   			   
-	   			   total += points;
-	   			   
-	   			   //Add to Quiz and Questions Objects
-	   			   QuestionsVO quest = new QuestionsVO(questionId, points, question, ans, choice1, choice2, choice3);
-	   			   quizQuestions.add(quest);
-	   			   
-	   			   //QuizDB results
-	   			   if(result.getRow()==1) {
-		   		   quizName = result.getString("quizTitle");
-				   instruction = result.getString("quizInstruction");
-				   scheduledDate = result.getDate("quizScheduledDate");
-				   graded = result.getBoolean("isGraded"); 
-	   			   }
-	   		   }
-	   		   
-	   		   if (scheduledDate.before(today)) {
-	   			   isAfter = true;
-	   		   }
-	   		   
-	   		   QuizVO quizInfo = new QuizVO(quizId, quizName);
-	   		   
-	   		   //Add Quiz info to Session attributes
-	   		   session.setAttribute("Id", quizId);
-	   		   session.setAttribute("Grade", graded);
-	   		   session.setAttribute("Schedule", scheduledDate);
-	   		   session.setAttribute("Directions", instruction);
-	   		   session.setAttribute("isAfter", isAfter);
-	   		   session.setAttribute("QuizQuestions",quizQuestions);
-	   		   session.setAttribute("quizInfo", quizInfo);
-	   		   session.setAttribute("Total", total);
-	   		
-	   		   res.sendRedirect(req.getContextPath()+"/viewQuiz.ftl");
+	    	   	   ViewQuizDAOBean bean = new ViewQuizDAOBean();
+	    	   	   QuizVO quiz = bean.getQuizInfo(quizId);
+	    	   	   quizQuestions = bean.getQuestionsInfo(quizId);
+	    	   	   
+	    	   	   for (int i = 0; i < quizQuestions.size(); i++) {
+	    	   		 int points = quizQuestions.get(i).getTotalPoints();
+	    	   		 total += points;
+	    	   	   }
+	    	   	   
+	    	   	   Date scheduledDate = quiz.getQuizScheduledDate();
+	    	   	   if (scheduledDate.before(today)) {
+		   			   isAfter = true;
+	    	   	   }
+	    	   	   
+	    	   	  // QuizVO quizInfo = new QuizVO(quizId, quizName);
+		   		   
+		   		   //Add Quiz info to Session attributes
+		   		   session.setAttribute("Id", quizId);
+		   		   session.setAttribute("Grade",quiz.isGraded());
+		   		   session.setAttribute("Schedule", quiz.getQuizScheduledDate());
+		   		   session.setAttribute("Directions", quiz.getQuizInstruction());
+		   		   session.setAttribute("isAfter", isAfter);
+		   		   session.setAttribute("QuizQuestions",quizQuestions);
+		   		   session.setAttribute("Name", quiz.getQuizTitle());
+		   		   session.setAttribute("Total", total);
+	       
+		   		   res.sendRedirect(req.getContextPath()+"/viewQuiz.ftl");
 	       }catch(Exception e) {
-	    	   e.printStackTrace();
+	    	   	   e.printStackTrace();
 	       }
 	}
 }
