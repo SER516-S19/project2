c5da23b0e3b9888e9050e993ae5c40e8f09cfe26
Darshan Prakash <darshan@darshans-mbp-3.3521.dhcp.asu.edu>
2019-02-22 16:52:24 -0700

    Created type check and connection to professor portal

--- Team_74/UI/quizzer/src/pages/Home/Home.js
+++ Team_74/UI/quizzer/src/pages/Home/Home.js
@@ -5,19 +5,53 @@ import ListGroupItem from "react-bootstrap/ListGroupItem";
 import ListGroup from "react-bootstrap/ListGroup";
 
 class Home extends Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+        // the initial application state
+        this.state = {
+            type: 0
+        }
+    }
+
     render() {
         return (
             <div className="Home">
-                <center>
-                    <h3>Student Portal</h3>
-                    <h5>Click on the Quiz to be completed</h5>
-                    <br></br></center>
+                {
+                    (parseInt(localStorage.getItem('type'),10) === 0)?
+                        <div>
+                                <center>
+                                    <h3>Student Portal</h3>
+                                    <h5>Click on the Quiz to be completed</h5>
+                                    <br></br></center>
 
-                <ListGroup className="QuizList">
-                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 1</NavLink></li></ListGroupItem>
-                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 2</NavLink></li></ListGroupItem>
-                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 3</NavLink></li></ListGroupItem>
-                </ListGroup>
+                                <ListGroup className="QuizList">
+                                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 1</NavLink></li></ListGroupItem>
+                                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 2</NavLink></li></ListGroupItem>
+                                    <ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 3</NavLink></li></ListGroupItem>
+                                </ListGroup>
+                            </div>
+                    :
+
+                        <div>
+                            <button type="button" className="Prof-Button" >Professor</button>
+                            <center>
+                                <h3>Professor Portal</h3>
+                                <h5>Select Quiz to be edited</h5>
+                                <br></br></center>
+
+                            {/*<ListGroup className="QuizList">*/}
+                                {/*<ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 1</NavLink></li></ListGroupItem>*/}
+                                {/*<ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 2</NavLink></li></ListGroupItem>*/}
+                                {/*<ListGroupItem><li><NavLink exact activeClassName="current" to='/takeQuiz'>Quiz 3</NavLink></li></ListGroupItem>*/}
+                            {/*</ListGroup>*/}
+
+
+                        </div>
+
+
+                }
             </div>
         );
     }
