8dc205195ce3b7e09722ce3942f53ae413bcacb8
Sajith Thattazhi <sthattaz@asu.edu>
2019-02-23 13:50:31 -0700

    Format issues fixed for Database Files.

--- Team_27/student_project2.sql
+++ Team_27/student_project2.sql
@@ -1,28 +1,32 @@
-CREATE DATABASE IF NOT EXISTS `project2`;
+CREATE DATABASE IF NOT EXISTS `project2_team27`;
 
-USE `project2`;
+USE `project2_team27`;
 
-CREATE TABLE 	`Student`(
-`student_id`	INT(10) PRIMARY KEY,
-`first_name`	VARCHAR(20),
-`last_name`		VARCHAR(20),
-`email_id`		VARCHAR(30));
+CREATE TABLE 	`Student`
+(
+	`student_id`	INT(10) PRIMARY KEY,
+	`first_name`	VARCHAR(20),
+	`last_name`		VARCHAR(20),
+	`email_id`		VARCHAR(30)
+);
 
-CREATE TABLE 	`Quiz_Attempt`(
+CREATE TABLE 	`Quiz_Attempt`
+(
 
-FOREIGN KEY 	fk_quiz_id(quiz_id)
-REFERENCES 		`Quiz`(quiz_id)
-ON UPDATE CASCADE
-ON DELETE RESTRICT,
+	FOREIGN KEY 	fk_quiz_id(quiz_id)
+	REFERENCES 		`Quiz`(quiz_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
 
-FOREIGN KEY 	fk_student_id(student_id)
-REFERENCES 		`Student`(student_id)
-ON UPDATE CASCADE
-ON DELETE RESTRICT,
+	FOREIGN KEY 	fk_student_id(student_id)
+	REFERENCES 		`Student`(student_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
 
-FOREIGN KEY 	fk_question_id(question_id)
-REFERENCES 		`Questions`(question_id)
-ON UPDATE CASCADE
-ON DELETE RESTRICT,
+	FOREIGN KEY 	fk_question_id(question_id)
+	REFERENCES 		`Questions`(question_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
 
-response VARCHAR(10));
+	response VARCHAR(10)
+);
