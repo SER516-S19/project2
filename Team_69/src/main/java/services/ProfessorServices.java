b74a14ca0d3eacee3d3f28854247e5f3fb588164
jspatel5 <jspatel5@asu.edu>
2019-02-22 22:30:29 -0700

    Adding display quiz data module.

--- Team_69/src/main/java/services/ProfessorServices.java
+++ Team_69/src/main/java/services/ProfessorServices.java
@@ -1,8 +1,11 @@
 package services;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
+import bean.Answer;
+import bean.Question;
 import bean.Quiz;
 import dao.ProfessorDAO;
 
@@ -17,6 +20,35 @@ public class ProfessorServices {
 	
 	public void publishQuiz(int quiz_id) {
 		professorDAO.publishQuiz(quiz_id);
+		
+	}
+	
+	public List<Question> getAllQuestionFromQuizID(int quizid){
+		
+		List<Question> questions =  professorDAO.getAllQuestionFromQuizID(quizid);
+		return questions;
+		
+	}
+	
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public List getAllAnswersFromQueList(List<Question> questions) {
+		
+		List questionData = new ArrayList<>();	
+		
+		for(Question question : questions) {
+			int queID = question.getQuestionId();
+			List questionInfo = new ArrayList<>();
+			questionInfo.add(question.getQuestion());
+			questionInfo.add(question.getPoints());
+			List<Answer> answers = professorDAO.getAllAnswersFromQuestionID(queID);
+			questionInfo.add(answers);
+			questionInfo.add(question.getQuestionId());
+			questionData.add(questionInfo);
+		}
+		
+		
+		return questionData;	
 		
 	}
 	
