3e22b4b05d2ca1bef0969bf6302f038bb93b7759
Sajith Thattazhi <sthattaz@asu.edu>
2019-02-23 14:04:52 -0700

    Indentation issues fixed.

--- Team_27/student_project2.sql
+++ Team_27/student_project2.sql
@@ -6,25 +6,25 @@ CREATE TABLE 	`Student`
 (
 	`student_id`	INT(10) PRIMARY KEY,
 	`first_name`	VARCHAR(20),
-	`last_name`		VARCHAR(20),
-	`email_id`		VARCHAR(30)
+	`last_name`	VARCHAR(20),
+	`email_id`	VARCHAR(30)
 );
 
 CREATE TABLE 	`Quiz_Attempt`
 (
 
 	FOREIGN KEY 	fk_quiz_id(quiz_id)
-	REFERENCES 		`Quiz`(quiz_id)
+	REFERENCES 	`Quiz`(quiz_id)
 	ON UPDATE CASCADE
 	ON DELETE RESTRICT,
 
 	FOREIGN KEY 	fk_student_id(student_id)
-	REFERENCES 		`Student`(student_id)
+	REFERENCES 	`Student`(student_id)
 	ON UPDATE CASCADE
 	ON DELETE RESTRICT,
 
 	FOREIGN KEY 	fk_question_id(question_id)
-	REFERENCES 		`Questions`(question_id)
+	REFERENCES 	`Questions`(question_id)
 	ON UPDATE CASCADE
 	ON DELETE RESTRICT,
 
