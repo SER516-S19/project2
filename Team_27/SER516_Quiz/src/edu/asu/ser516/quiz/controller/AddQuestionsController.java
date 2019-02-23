7bcabf0b6b2407ced13881ed4654a09a45396cc5
bpanda1 <bpanda1@asu.edu>
2019-02-22 23:02:30 -0700

    Fixed Minor type conversion errors

--- Team_27/SER516_Quiz/src/edu/asu/ser516/quiz/controller/AddQuestionsController.java
+++ Team_27/SER516_Quiz/src/edu/asu/ser516/quiz/controller/AddQuestionsController.java
@@ -1,15 +1,12 @@
 package edu.asu.ser516.quiz.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import edu.asu.ser516.quiz.model.Question;
 import edu.asu.ser516.quiz.dao.QuestionsDao;
@@ -25,8 +22,6 @@ public class AddQuestionsController extends HttpServlet {
 
 		Enumeration paramNames = request.getParameterNames();
 
-		Boolean questionAddedSuccessfully = true;
-
 		try {
 			while (paramNames.hasMoreElements()) {
 				String paramName = (String) paramNames.nextElement();
@@ -35,48 +30,38 @@ public class AddQuestionsController extends HttpServlet {
 
 					String questionNumber = paramName.substring(8);
 
-					String questionText = request.getParameterValues("question" + questionNumber);
+					String questionText = request.getParameter("question" + questionNumber);
 
-					String optionA = request.getParameterValues("OptionAForQues" + questionNumber);
-					String optionB = request.getParameterValues("OptionBForQues" + questionNumber);
-					String optionC = request.getParameterValues("OptionCForQues" + questionNumber);
-					String optionD = request.getParameterValues("OptionDForQues" + questionNumber);
+					String optionA = request.getParameter("OptionAForQues" + questionNumber);
+					String optionB = request.getParameter("OptionBForQues" + questionNumber);
+					String optionC = request.getParameter("OptionCForQues" + questionNumber);
+					String optionD = request.getParameter("OptionDForQues" + questionNumber);
 
-					String tmp = request.getParameterValues("isOptionACorrectForQues" + questionNumber);
+					String tmp = request.getParameter("isOptionACorrectForQues" + questionNumber);
 					Boolean isOptionACorrect = (tmp == null) ? false : true;
-					tmp = request.getParameterValues("isOptionBCorrectForQues" + questionNumber);
+					tmp = request.getParameter("isOptionBCorrectForQues" + questionNumber);
 					Boolean isOptionBCorrect = (tmp == null) ? false : true;
-					tmp = request.getParameterValues("isOptionCCorrectForQues" + questionNumber);
+					tmp = request.getParameter("isOptionCCorrectForQues" + questionNumber);
 					Boolean isOptionCCorrect = (tmp == null) ? false : true;
-					tmp = request.getParameterValues("isOptionDCorrectForQues" + questionNumber);
+					tmp = request.getParameter("isOptionDCorrectForQues" + questionNumber);
 					Boolean isOptionDCorrect = (tmp == null) ? false : true;
 
-					Integer points = request.getParameterValues("PointsForQues" + questionNumber);
-					tmp = request.getParameterValues("isMultipleAnswerQues" + questionNumber);
+					String points = request.getParameter("PointsForQues" + questionNumber);
+					tmp = request.getParameter("isMultipleAnswerQues" + questionNumber);
 					Boolean isMultipleAnswer = (tmp == null) ? false : true;
 
 					Question questionModel = new Question(questionText, optionA, optionB, optionC, optionD, isOptionACorrect,
 							isOptionBCorrect, isOptionCCorrect, isOptionDCorrect, points, isMultipleAnswer);
 
-					Boolean isAdded = QuestionsDao.insert(questionModel);
+					QuestionsDao.insert(questionModel);
 
-					if (isInserted)
-						req.getRequestDispatcher("Success.html").forward(req, res);
-					else {
-						questionAddedSuccessfully = false;
-						break;
-					}
 				}
 			}
 		}
 		catch (Exception exc) {
-			res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Java Exception at Server");
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Java Exception at Server");
 			exc.printStackTrace();
-		}
-
-		if (!questionAddedSuccessfully)
-			res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Qustion(s) can not be added !");
-			
+		}	
 	}
 
 	// Method to handle POST method request.
