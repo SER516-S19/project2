01a3acb0e957abffe109ca477c5454795c053de1
ankitaShivanand <ankitashivanand25@gmail.com>
2019-02-22 18:35:53 -0700

    #US 06Task21 Answer options

--- Team_04/src/student/dto/AnswerOption.java
+++ Team_04/src/student/dto/AnswerOption.java
@@ -3,9 +3,20 @@ package student.dto;
 public class AnswerOption {
     private String ans_desc;
     private long ans_id;
-    public AnswerOption(long ans_id, String ans_desc) {
+    private boolean is_correct;
+    public AnswerOption(long ans_id, String ans_desc, boolean is_correct) {
         this.ans_desc = ans_desc;
         this.ans_id = ans_id;
+        this.is_correct = is_correct;
+    }
+
+
+    public boolean getIsCorrect() {
+        return this.is_correct;
+    }
+
+    public void setIs_correct(boolean is_correct) {
+        this.is_correct = is_correct;
     }
 
     public long getAns_id() {
