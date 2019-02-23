bf393870f2f126b27baed33f7979365a16d686d7
LAZZZZZZZy <wjyhz@hotmail.com>
2019-02-23 03:34:59 -0700

    Update myquizzes.js

--- Team_47/QuizCreator/WebContent/js/myquizzes.js
+++ Team_47/QuizCreator/WebContent/js/myquizzes.js
@@ -4,7 +4,7 @@ var rows = document.getElementsByTagName("tr");
     for (var i = 0; i < rows.length; i++) {
         rows[i].onclick = function() {
 //           alert("You have chose Quiz" + i + ".");
-           window.location.href = '/questions.jsp';
+           window.location.href = '/instruction.jsp';
         };
     }
 }
