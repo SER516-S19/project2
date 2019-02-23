1534c81cce5412313f7e2c240c5faa82dcc51781
Hari Krishnan <hari4nadal@gmail.com>
2019-02-22 22:50:13 -0700

    handled exception

--- Team_04/src/content/creator/helper/ViewContentListHelper.java
+++ Team_04/src/content/creator/helper/ViewContentListHelper.java
@@ -2,6 +2,7 @@ package content.creator.helper;
 
 import content.creator.operations.DataOps;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public final class ViewContentListHelper {
     private ViewContentListHelper() {
     }
 
-    public static List<Integer> getQuizList() {
+    public static List<Integer> getQuizList() throws SQLException {
         String tableName = "quiz_content";
         String colName = "quiz_id";
         List<Integer> list = new ArrayList<>();
