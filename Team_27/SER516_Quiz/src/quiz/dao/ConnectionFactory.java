90e87820a1b93906eb6de7db6823e01f81bcb96c
shefaliAJ7 <sanand22@asu.edu>
2019-02-23 00:28:19 -0700

    US#3-TASK#49: Adding Exceptions

---
+++ Team_27/SER516_Quiz/src/quiz/dao/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package quiz.dao;
+
+import java.util.Properties;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.lang.reflect.Constructor;
+
+/**
+ * This factory returns connections based on which source is responsible
+ * for a given object (table).
+ */
+public class ConnectionFactory {
+    // This holds properties
+    private static Properties dbProperties = new Properties();
+
+    static {
+	try {
+	    dbProperties.load(ConnectionFactory.class.getClassLoader().getResourceAsStream("rdbm.properties"));
+	    Class.forName(dbProperties.getProperty("mysql_jdbcDriver"));
+	} catch (Throwable t) {
+	    t.printStackTrace();
+	} finally {
+	}
+    }
+
+    
+  // We can and should replace with a Connection pool
+  public static final Connection getConnection() {
+ // 	throws DataAccessException {
+
+	  String driverURL = dbProperties.getProperty("mysql_jdbcURL");
+	  String dbUser = dbProperties.getProperty("mysql_jdbcUser");
+	  String dbPass = dbProperties.getProperty("mysql_jdbcPwd");
+      try {
+          return DriverManager.getConnection(driverURL, dbUser, dbPass);
+      }
+      catch(SQLException s) {
+    	  s.printStackTrace();
+      }
+      /*
+      catch (Throwable t) {
+          throw new DataAccessException("Could not get connection for " + driverURL + ", " + dbUser + ", " + dbPass);
+      }*/
+	return null;
+  }
+}
---
+++ Team_27/SER516_Quiz/src/quiz/exceptions/DataAccessException.java
@@ -0,0 +1,19 @@
+/*
+ * Taken from J2EE Design Patterns Applied. Modified slightly.
+ */
+
+package quiz.exceptions;
+
+@SuppressWarnings("serial")
+public class DataAccessException extends Exception{
+  Throwable exceptionCause = null;
+
+  public DataAccessException(String pExceptionMsg){
+    super(pExceptionMsg);
+  }
+
+  public DataAccessException(String pExceptionMsg, Throwable pException){
+    super(pExceptionMsg);
+    this.exceptionCause=pException;
+  }
+}
