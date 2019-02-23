bd1fb9c36185d782b8b1bb1c8f9dbfa0c0443a23
dlahtine <dlahtine@asu.edu>
2019-02-22 23:14:47 -0700

    wip on the submission endpoint

--- Team_47/QuizCreator/src/main/java/com/asu/ser516/team47/servlet/SubmissionServlet.java
+++ Team_47/QuizCreator/src/main/java/com/asu/ser516/team47/servlet/SubmissionServlet.java
@@ -1,6 +1,7 @@
 package com.asu.ser516.team47.servlet;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -15,5 +16,30 @@ public class SubmissionServlet extends HttpServlet {
         out.write("hello from a servlet".getBytes());
         out.flush();
         out.close();
+    }
+
+    @Override
+    void doPost(HttpServletRequest request , HttpServletResponse response){
+        String htmlCode = "204";
+        String htmlMessage;
+        //Mandatory fields to create a submission entry
+        String quizIdField;
+        String enrollIdField;
+        String timetaken;
+        String attempt;
+
+        Enumeration paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()){
+            String paramName = (String)paramNames.nextElement();
+            if (paramName.equals("quiz_id")){
+
+            } else if (paramName.equals("enroll_id")){
+
+            } else if (paramName.equals("attempt")){
+
+            } else if (paramName.equals("timeTaken")){
+
+            } else if (paramName.length() >= "choice_fk".length()){}
+        }
     }
 }
