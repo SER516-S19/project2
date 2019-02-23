854f4f9e4a6912330c8ae87b0f456ca2697c9c77
Aprajita Thakur <athaku20@asu.edu>
2019-02-22 18:39:15 -0700

    Refactored method names as per Java Standard in Student Module

--- Team_04/src/student/dto/QuizContent.java
+++ Team_04/src/student/dto/QuizContent.java
@@ -12,77 +12,77 @@ public class QuizContent {
   private String ques_desc;
   private long ans_id;
   private String ans_desc;
-  private String is_correct;
+  private boolean is_correct;
   private long max_score;
 
-  public long getQuiz_id() {
+  public long getQuizId() {
     return quiz_id;
   }
 
-  public void setQuiz_id(long quiz_id) {
+  public void setQuizId(long quiz_id) {
     this.quiz_id = quiz_id;
   }
 
 
-  public long getQues_id() {
+  public long getQuesId() {
     return ques_id;
   }
 
-  public void setQues_id(long ques_id) {
+  public void setQuesId(long ques_id) {
     this.ques_id = ques_id;
   }
 
 
-  public String getQues_type() {
+  public String getQuesType() {
     return ques_type;
   }
 
-  public void setQues_type(String ques_type) {
+  public void setQuesType(String ques_type) {
     this.ques_type = ques_type;
   }
 
 
-  public String getQues_desc() {
+  public String getQuesDesc() {
     return ques_desc;
   }
 
-  public void setQues_desc(String ques_desc) {
+  public void setQuesDesc(String ques_desc) {
     this.ques_desc = ques_desc;
   }
 
 
-  public long getAns_id() {
+  public long getAnsId() {
     return ans_id;
   }
 
-  public void setAns_id(long ans_id) {
+  public void setAnsId(long ans_id) {
     this.ans_id = ans_id;
   }
 
 
-  public String getAns_desc() {
+  public String getAnsDesc() {
     return ans_desc;
   }
 
-  public void setAns_desc(String ans_desc) {
+  public void setAnsDesc(String ans_desc) {
     this.ans_desc = ans_desc;
   }
 
 
-  public String getIs_correct() {
+  public boolean isIsCorrect() {
     return is_correct;
   }
 
-  public void setIs_correct(String is_correct) {
+  public void setIsCorrect(boolean is_correct) {
     this.is_correct = is_correct;
   }
 
 
-  public long getMax_score() {
+  public long getMaxScore() {
     return max_score;
   }
 
-  public void setMax_score(long max_score) {
+  public void setMaxScore(long max_score) {
     this.max_score = max_score;
   }
 
