0be9dc272d7f6fea1b9398b8491004a2fa0bf3e0
Sumanth Paranjape <sparanj2@asu.edu>
2019-02-24 00:48:12 -0700

    Merging student and professor entities in sql file

--- Team_27/SER516_Quiz/sql/Dump20190223.sql
+++ Team_27/SER516_Quiz/sql/Dump20190223.sql
@@ -93,4 +93,33 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+CREATE TABLE 	`Student`
+(
+	`student_id`	INT(10) PRIMARY KEY,
+	`first_name`	VARCHAR(20),
+	`last_name`	VARCHAR(20),
+	`email_id`	VARCHAR(30)
+);
+
+CREATE TABLE 	`Quiz_Attempt`
+(
+
+	FOREIGN KEY 	fk_quiz_id(quiz_id)
+	REFERENCES 	`Quiz`(quiz_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+
+	FOREIGN KEY 	fk_student_id(student_id)
+	REFERENCES 	`Student`(student_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+
+	FOREIGN KEY 	fk_question_id(question_id)
+	REFERENCES 	`Questions`(question_id)
+	ON UPDATE CASCADE
+	ON DELETE RESTRICT,
+
+	response VARCHAR(10)
+);
+
 -- Dump completed on 2019-02-23 13:19:19
