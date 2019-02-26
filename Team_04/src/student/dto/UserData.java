7072360eaecf017e8913e9b819709c897ceb3eaf
hkolli <hkolli@asu.edu>
2019-02-26 13:16:16 -0700

    US(#58) TASK(#65) Create database for user data and updated dto class.

--- Team_04/src/student/dto/UserData.java
+++ Team_04/src/student/dto/UserData.java
@@ -1,8 +1,49 @@
 package student.dto;
 
 public class UserData {
+
     private String userName;
     private String password;
     private String userType;
-    private String userId;
+    private double userId;
+
+    public UserData(String userName, String password, String userType, double userId) {
+        this.userName = userName;
+        this.password = password;
+        this.userType = userType;
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public double getUserId() {
+        return userId;
+    }
+
+    public void setUserId(double userId) {
+        this.userId = userId;
+    }
+
 }
