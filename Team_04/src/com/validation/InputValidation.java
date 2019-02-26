06e967f1bbf2107cb366d17f3cea0ca008214360
ankitaShivanand <ankitashivanand25@gmail.com>
2019-02-25 20:54:49 -0700

    US#57 Task#68 "code for login validation"

---
+++ Team_04/src/com/validation/InputValidation.java
@@ -0,0 +1,16 @@
+package com.validation;
+
+public class InputValidation {
+    public void loginValidation(String username, String password)
+    {
+        if (username.equalsIgnoreCase("") || (password.equalsIgnoreCase(""))
+        ) {
+            System.out.println("Username");
+
+        }
+        else
+        {
+            System.out.println("Password");
+        }
+    }
+}
