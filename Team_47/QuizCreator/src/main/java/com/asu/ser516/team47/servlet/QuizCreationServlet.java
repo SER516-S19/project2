499d1bb04157bb221ab365d36ec6c50d3b1d4af2
dlahtine <dlahtine@asu.edu>
2019-02-28 11:04:21 -0700

    Submission Servlet has been updated to accept the json objects discussed. Still needs to be tested, and attempts and time limits still need to be changed to be enforced server-side.

---
+++ Team_47/QuizCreator/src/main/java/com/asu/ser516/team47/servlet/QuizCreationServlet.java
@@ -0,0 +1,20 @@
+package com.asu.ser516.team47.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Endpoint for professor to create a quiz.
+ *
+ * @author David Lahtinen
+ * @version 1.0
+ * @since 02/28/19
+ */
+@WebServlet(name = "QuizCreationServlet")
+public class QuizCreationServlet extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse res){
+
+    }
+}
