95477f436bd6caa41e3026b567124e0742254033
xiangwei <xzheng80@asu.edu>
2019-03-01 02:57:54 -0700

    update StudentController for quizinstruct

--- Team_76/src/Team76/Controller/StudentController.java
+++ Team_76/src/Team76/Controller/StudentController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Team76.Entity.QuizEntity;
+import Team76.Utilities.QuizInstructModel;
 import Team76.Utilities.StudentQuizModel;
 
 /**
@@ -29,7 +30,7 @@ public class StudentController extends HttpServlet {
 		if (action == null || action.isEmpty()) {
 			System.out.println("**** no acton");
 			response.sendRedirect("login.jsp");
-		} else if (action.equalsIgnoreCase("AttemptQuiz")) {
+		} /*else if (action.equalsIgnoreCase("AttemptQuiz")) {
 			System.out.println("**** attempt quiz");
 			List<QuizEntity> quizzes = null;
 			StudentQuizModel AttemptQuiz;
@@ -41,10 +42,36 @@ public class StudentController extends HttpServlet {
 			}			
 			request.getSession().setAttribute("quizzes", quizzes);
 			response.sendRedirect("StudentsQuiz.jsp");
-		} else if (action.equalsIgnoreCase("StartQuiz")) {
+		            } */else if (action.equalsIgnoreCase("AttemptQuiz")) {			
+			      //} else if (action.equalsIgnoreCase("StartQuiz")) {//TEST
+			//Xiangwei
 			System.out.println("**** start quiz");
 			String quizId = request.getParameter("quizId");
 			System.out.println("**** quiz id: " + quizId);
+			QuizEntity quiztaken = null;
+			QuizInstructModel QuizInstruct;
+			try {
+				QuizInstruct = new QuizInstructModel();
+				quiztaken = QuizInstruct.getquiz("1");
+			}catch (Exception e) {
+				e.printStackTrace();
+			}			
+			request.getSession().setAttribute("quiztaken", quiztaken);
+			response.sendRedirect("QuizInstruct.jsp");	
+			//Xiangwei
+			/*System.out.println("**** start quiz");
+			String quizId = request.getParameter("quizId");
+			System.out.println("**** quiz id: " + quizId);
+			QuizEntity quiztaken = null;
+			QuizInstructModel QuizInstruct;
+			try {
+				QuizInstruct = new QuizInstructModel();
+				quiztaken = QuizInstruct.getquiz(quizId);
+			}catch (Exception e) {
+				e.printStackTrace();
+			}			
+			request.getSession().setAttribute("quiztaken", quiztaken);
+			response.sendRedirect("QuizInstruct.jsp");	*/
 		} else {
 			response.getWriter().println("<font color=red>Something went wrong please login again.</font>");
 			response.sendRedirect("login.jsp");
