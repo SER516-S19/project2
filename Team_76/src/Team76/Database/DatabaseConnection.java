0de79cf349f79bec71f9d4356586c3d1b30d9b05
xiangwei <xzheng80@asu.edu>
2019-03-01 02:58:17 -0700

    fix DataConnection disconnect

--- Team_76/src/Team76/Database/DatabaseConnection.java
+++ Team_76/src/Team76/Database/DatabaseConnection.java
@@ -2,14 +2,27 @@ package Team76.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DatabaseConnection {
 
 	public Connection establishConnection() throws Exception {
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ser516p2?useSSL=false", "root",
-				"pass123");
-		return con;
+		//Class.forName("com.mysql.cj.jdbc.Driver");
+		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ser516p2?useSSL=false", "root",
+		//		"199021");//pass123
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			Class.forName(driver);
+			} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+		Connection connection = null;	
+		String connectionUrl = "jdbc:mysql://localhost:3306/";
+		String database = "ser516p2";
+		String userid = "root";
+		String password = "199021";
+		connection = DriverManager.getConnection(connectionUrl+database, userid, password);
+		return connection;
 	}
 }
