b13d0cf21174e604fc2038fb497848ba220ca7eb
Suraj Atmakuri <surajatmakuri@hotmail.com>
2019-02-28 17:39:49 -0700

    added more functionality to the quiz creator UI

--- Team_47/QuizCreator/WebContent/js/quizcreator.js
+++ Team_47/QuizCreator/WebContent/js/quizcreator.js
@@ -1,32 +1,63 @@
 /** 
-  * Author(s): Meng-Ze Chen, Jiayan Wang
+  * Author(s): Suraj Atmakuri
   * Date: 2019/2/27
   * Description: Page for creating quizzes
   */
-var i = 1;
-function addFields(thisElem, nameTracingList) {
-    var divX = thisElem.parentNode;
-    divX.innerHTML = divX.innerHTML + "<br>"
-    divX.innerHTML = divX.innerHTML + "question:<br>"
-    questionChoiceIterator += 1;
-    var questionID = `question-${questionChoiceIterator}`
-    divX.innerHTML = divX.innerHTML + `<input type='text' name='${questionID}' value='enter the question' size='30'>`
-    divX.innerHTML = divX.innerHTML + "<br> Select the check boxes for correct answers <br>"
-    nameTracingList.push(questionID)
-    addAnswerFields(divX, nameTracingList);
-}
-function addAnswerFields(divX, nameTracingList) {
-    var possibleChoices = ['a', 'b', 'c', 'd', 'e'];
-    for (var idx = 0; idx < possibleChoices.length; idx += 1) {
-        var checkboxID = `checkbox-${possibleChoices[idx]}-${questionChoiceIterator}`
-        var choiceDescID = `choice-desc-${possibleChoices[idx]}-${questionChoiceIterator}`
-        divX.innerHTML = divX.innerHTML + `<input type='checkbox' name='${checkboxID}' value='${possibleChoices[idx]}'>`
-        divX.innerHTML = divX.innerHTML + `${possibleChoices[idx]}.`
-        divX.innerHTML = divX.innerHTML + `<input type='text' value='enter the answer' name='${choiceDescID}' size=30><br>`
-
-        nameTracingList.push(checkboxID)
-        nameTracingList.push(choiceDescID)
+var i=1;
+    function addFields()
+    {
+      div1.innerHTML = div1.innerHTML + ("Q"+ i + ":")
+      var id = "q";
+      var id_var = id.concat(i.toString());
+      div1.innerHTML = div1.innerHTML + "<input type='text' id="+ id_var +" name='question' value='enter the question'size='30'+i><br>"
+      selectType(id_var);
+      i++;
     }
-    divX.innerHTML = divX.innerHTML + "credits:<input type='text' name='points' value='enter the points' size=2>"
-    divX.innerHTML = divX.innerHTML + "<br>"
-}
+    function selectType(a)
+    {
+      var j=0;
+      var radio1 = a.concat("_radio1");
+      var radio2 = a.concat("_radio2");
+      var button_id = a.concat("_button")
+      div1.innerHTML = div1.innerHTML + "select the number of answers: <br><input type='radio' id="+ radio1 +" name='a' value='sa'> single answer <br>" 
+      div1.innerHTML = div1.innerHTML + "<input type='radio' id="+ radio2 +" name='a' value='ma'>multiple answers <br>"
+      div1.innerHTML = div1.innerHTML + "<input type='button' id="+button_id+" onclick='validateType("+radio1+","+radio2+","+button_id+")' value='add answers'><br>"
+    }
+    function validateType(rad_a,rad_b,button_id)
+    {
+     if(rad_a.checked  || rad_b.checked) 
+      {
+        if(rad_a.checked)
+         {
+           addAnswerFields(1,rad_a);
+          }
+        else if(rad_b.checked)
+                {
+                 addAnswerFields(2,rad_b);
+                }
+      button_id.style.visibility = false;
+     }
+    }
+    function addAnswerFields(a,ans)
+    {
+      var opt_a = ans.id.concat("_a");
+      var opt_b = ans.id.concat("_b");
+      var opt_c = ans.id.concat("_c");
+      var opt_d = ans.id.concat("_d");
+      var val = ans.id.concat("_cred");
+      if(a==1)
+       {
+          div1.innerHTML = div1.innerHTML + "<input type='radio' id="+ opt_a +" name='choice' value='a'>&#97.<input type='text' value='enter the answer' name='a'size=30><br>"
+          div1.innerHTML = div1.innerHTML + "<input type='radio' id="+ opt_b +" name='choice' value='b'>&#98.<input type='text' value='enter the answer' name='b'size=30><br>"
+          div1.innerHTML = div1.innerHTML + "<input type='radio' id="+ opt_c +" name='choice' value='c'>&#99.<input type='text' value='enter the answer' name='c'size=30><br>"
+          div1.innerHTML = div1.innerHTML + "<input type='radio' id="+ opt_d +" name='choice' value='d'>&#100.<input type='text' value='enter the answer' name='d'size=30><br>"
+       }
+      else if(a==2)
+             {
+               div1.innerHTML = div1.innerHTML + "<input type='checkbox' id="+ opt_a +" name='choice' value='a'>&#97.<input type='text' value='enter the answer' name='a'size=30><br>"
+               div1.innerHTML = div1.innerHTML + "<input type='checkbox' id="+ opt_b +" name='choice' value='b'>&#98.<input type='text' value='enter the answer' name='b'size=30><br>"
+               div1.innerHTML = div1.innerHTML + "<input type='checkbox' id="+ opt_c +" name='choice' value='c'>&#99.<input type='text' value='enter the answer' name='c'size=30><br>"
+               div1.innerHTML = div1.innerHTML + "<input type='checkbox' id="+ opt_d +" name='choice' value='d'>&#100.<input type='text' value='enter the answer' name='d'size=30><br>"
+             }
+      div1.innerHTML = div1.innerHTML + "credits:<input type='text'id="+ val +" name='points' value='enter the points'size=2>"
+    }
--- Team_47/QuizCreator/WebContent/quizcreator.jsp
+++ Team_47/QuizCreator/WebContent/quizcreator.jsp
@@ -1,5 +1,5 @@
 <!-- 
-  - Author(s): Meng-Ze Chen, Jiayan Wang
+  - Author(s): Suraj Atmakuri
   - Date: 2019/2/20
   - Description: Page for creating quizzes
   -->
@@ -8,44 +8,53 @@ pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 
-<head>
+ <head>
     <meta charset="UTF-8">
     <meta id="request-method" name="request-method" content="GET">
-    <meta name="author" content="Meng-Ze Chen, Jiayan Wang">
-    <meta name="copyright" content="© 2019 Meng-Ze Chen, Jiayan Wang. All Rights Reserved.">
+    <meta name="author" content="Suraj Atmakuri">
+    <meta name="copyright" content="© 2019 Suraj Atmakuri. All Rights Reserved.">
     <meta name="keywords" content="Quizzes Overview">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>
         Quiz editor
     </title>
-    <link rel="stylesheet" href="CSS/quizcreator.css" type="text/css" />
-    <script src="js/quizcreator.js"></script>
-</head>
+     <link rel="stylesheet" href="CSS/quizcreator.css" type="text/css" />
+     <script src="js/quizcreator.js"></script>
+ </head>
 
-<body>
-    <div id='top'>
-        <p><a href="login.jsp" class='button' align:"right">logout</a></p>
+ <body>
+ <div id='top'>
+   <p><a href="loginpage.jsp" class='button' align:"right">logout</a></p>
+ </div>
+ <div id='mid'>
+  <form action="summarypage.jsp">
+    quiz title: </br>
+    <input type='text' name='title' value='enter the title'size=30></br>
+    enter instructions:</br>
+    <input type='text' name='instructions' value='enter instructions'size=30></br>
+    enter the group of the quiz:</br>
+    <input type='text' name='group' value='group name' size=30></br>
+    enter the type of the quiz:
+    <select>
+      <option value='g'> graded </option>
+      <option value='ug'> ungraded </option>
+     </select> <br>
+    enter the start date:</br>
+    <input type='date' id='start' name='start_date' value='28-02-19' min='01-01-19' max='01-05-19'><br>
+    enter start time:</br> 
+    <input type='time' id='stime' name='start_time' value='13:30' min='00:00' max='23:59'><br>
+    enter the end date:</br>
+    <input type='date' id='end' name='end_date' value='29-02-19' min='02-01-19' max='07-05-19'><br>
+    enter end time:</br>
+    <input type='time' id='etime' name='end_time' value='1:30' min='00:00' max='23:59'><br>
+    enter the duration of the quiz:</br>
+    <input type='text' id='hrs' value='0' min='0' max='3'>hrs
+    <input type='text' id='min' value='0' min='0' max='59'>mins
+    <div id='div1'>
     </div>
-    <div id='mid'>
-        <form action="summarypage.jsp">
-            Quiz title:</br>
-            <input type='text' name='title' value='enter the title' size=30></br>
-            Instructions:</br>
-            <input type='text' name='instructions' value='enter instructions' size=30></br>
-            Group of the quiz:</br>
-            <input type='text' name='group' value='group name' size=30></br>
-            <div id='questionDiv'>
-                <script>
-                    var questionDict = {};
-                    var nameTracingList = [];
-                    var questionChoiceIterator = 0;
-                </script>
-                <input type='button' onclick='addFields(this, nameTracingList)' value='add question'>
-            </div>
-            <input id="beginbtn" type="submit" value="Submit">
-
-        </form>
-    </div>
-</body>
-
+    <input type='button' onclick='addFields()' value='add question'>
+    <input id = "beginbtn" type="submit" value="Submit">
+   </form>
+  </div>
+ </body>
 </html>
