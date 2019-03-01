68e3705bbc63dbc4f935c49288df0c11d2e6e668
Sabyasachi <mohantysabyasachi27@gmail.com>
2019-03-01 13:01:00 -0700

    Make UI look better - Added PieCharts, Horizontal Bar Charts to the Dashboard

--- Team_74/UI/quizzer/src/components/Dashboard/PiChart.js
+++ Team_74/UI/quizzer/src/components/Dashboard/PiChart.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { Pie } from 'react-chartjs-2';
 
 class PiChart extends Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+             data : {
+                labels: [
+                    'Red',
+                    'Green',
+                    'Yellow'
+                ],
+                datasets: [{
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56'
+                    ],
+                    hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56'
+                    ]
+                }]
+            }
+        };
     }
     
     render() {
         return (
             <div>
-                
+                <h2>Pie Example</h2>
+                <Pie data={this.state.data} />
             </div>
         );
     }
