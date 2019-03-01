de46cbceb9a524b07d1343a51f958fe9a5ccde92
Meng-Ze Chen <mchen149@asu.edu>
2019-03-01 10:31:11 -0700

    Add dynamic selection for multiple choice/multiple answers question

--- Team_47/QuizCreator/WebContent/js/quizcreator.js
+++ Team_47/QuizCreator/WebContent/js/quizcreator.js
@@ -1,32 +1,67 @@
 /** 
-  * Author(s): Meng-Ze Chen, Jiayan Wang
-  * Date: 2019/2/27
+  * Author(s): Meng-Ze Chen, Jiayan Wang, Suraj Atmakuri
+  * Date: 2019/2/20
   * Description: Page for creating quizzes
   */
-var i = 1;
+
 function addFields(thisElem, nameTracingList) {
-    var divX = thisElem.parentNode;
-    divX.innerHTML = divX.innerHTML + "<br>"
-    divX.innerHTML = divX.innerHTML + "question:<br>"
+    var divID = thisElem.parentNode;
+    divID.innerHTML = divID.innerHTML + "<br>"
+    divID.innerHTML = divID.innerHTML + "question:<br>"
     questionChoiceIterator += 1;
     var questionID = `question-${questionChoiceIterator}`
-    divX.innerHTML = divX.innerHTML + `<input type='text' name='${questionID}' value='enter the question' size='30'>`
-    divX.innerHTML = divX.innerHTML + "<br> Select the check boxes for correct answers <br>"
+    divID.innerHTML = divID.innerHTML + `<input type='text' name = 'question' id='${questionID}' value='enter the question' size='30'>`
+    divID.innerHTML = divID.innerHTML + "<br> Select the check boxes for correct answers <br>"
     nameTracingList.push(questionID)
-    addAnswerFields(divX, nameTracingList);
+    addAnswerFields(divID, nameTracingList);
 }
-function addAnswerFields(divX, nameTracingList) {
+
+function addAnswerFields(divID, nameTracingList) {
+    addQuestionTypeFields(divID)
     var possibleChoices = ['a', 'b', 'c', 'd', 'e'];
     for (var idx = 0; idx < possibleChoices.length; idx += 1) {
         var checkboxID = `checkbox-${possibleChoices[idx]}-${questionChoiceIterator}`
         var choiceDescID = `choice-desc-${possibleChoices[idx]}-${questionChoiceIterator}`
-        divX.innerHTML = divX.innerHTML + `<input type='checkbox' name='${checkboxID}' value='${possibleChoices[idx]}'>`
-        divX.innerHTML = divX.innerHTML + `${possibleChoices[idx]}.`
-        divX.innerHTML = divX.innerHTML + `<input type='text' value='enter the answer' name='${choiceDescID}' size=30><br>`
+        //divID.innerHTML = divID.innerHTML + `<input type='checkbox' name='checkbox' id='${checkboxID}' value='${possibleChoices[idx]}'>`
+
+        var checkboxForThisAns = document.createElement('input');
+        checkboxForThisAns.id = checkboxID;
+        checkboxForThisAns.name = 'checkbox'
+        checkboxForThisAns.type = 'checkbox'
+        checkboxForThisAns.value = `${possibleChoices[idx]}`
+        divID.appendChild(checkboxForThisAns);
+        
+        divID.innerHTML = divID.innerHTML + `${possibleChoices[idx]}.`
+        divID.innerHTML = divID.innerHTML + `<input type='text' value='enter the answer' name='choiceDesc' name='${choiceDescID}' size=30><br>`
 
         nameTracingList.push(checkboxID)
         nameTracingList.push(choiceDescID)
     }
-    divX.innerHTML = divX.innerHTML + "credits:<input type='text' name='points' value='enter the points' size=2>"
-    divX.innerHTML = divX.innerHTML + "<br>"
+    var creditBoxID = `pointBox-${questionChoiceIterator}`
+    divID.innerHTML = divID.innerHTML + `credits:<input type='text' name='points' id='${creditBoxID}' value='enter the points' size=2>`
+    divID.innerHTML = divID.innerHTML + "<br><br>"
+}
+
+function addQuestionTypeFields(divID) {
+    var multichoiceStr = 'multichoice_' + questionChoiceIterator
+    var multianswerStr = 'multianswer_' + questionChoiceIterator 
+    divID.innerHTML = divID.innerHTML + `Select question type: <br>`
+    divID.innerHTML = divID.innerHTML + `<input type='radio' id='${multichoiceStr}' onclick="selectQuestionType(this, '${multichoiceStr}')" name='answerType' value='singleAnswer'> Single Answer Question<br>`
+    divID.innerHTML = divID.innerHTML + `<input type='radio' id='${multianswerStr}' onclick="selectQuestionType(this, '${multianswerStr}')" name='answerType' value='multiAnswer'> Multiple Answers Question<br>`
+}
+
+function selectQuestionType(divID, answerTypeEncoded) {
+    answerTypeList = answerTypeEncoded.split('_')
+
+    questionID = answerTypeList[answerTypeList.length-1]
+    answerType = answerTypeList[0]
+    var possibleChoices = ['a', 'b', 'c', 'd', 'e'];
+    for (var i=0; i<possibleChoices.length; i+=1) {
+        var checkboxID = `checkbox-${possibleChoices[i]}-${questionID}`
+        if (answerType == 'multichoice') {
+            document.getElementById(checkboxID).type = 'radio'
+        } else {
+            document.getElementById(checkboxID).type = 'checkbox'
+        }
+    }
 }
--- Team_47/QuizCreator/WebContent/quizcreator.jsp
+++ Team_47/QuizCreator/WebContent/quizcreator.jsp
@@ -1,5 +1,5 @@
 <!-- 
-  - Author(s): Meng-Ze Chen, Jiayan Wang
+  - Author(s): Meng-Ze Chen, Jiayan Wang, Suraj Atmakuri
   - Date: 2019/2/20
   - Description: Page for creating quizzes
   -->
@@ -11,8 +11,8 @@ pageEncoding="UTF-8"%>
 <head>
     <meta charset="UTF-8">
     <meta id="request-method" name="request-method" content="GET">
-    <meta name="author" content="Meng-Ze Chen, Jiayan Wang">
-    <meta name="copyright" content="© 2019 Meng-Ze Chen, Jiayan Wang. All Rights Reserved.">
+    <meta name="author" content="Meng-Ze Chen, Jiayan Wang, Suraj Atmakuri">
+    <meta name="copyright" content="© 2019 Meng-Ze Chen, Jiayan Wang, Suraj Atmakuri. All Rights Reserved.">
     <meta name="keywords" content="Quizzes Overview">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>
@@ -34,13 +34,31 @@ pageEncoding="UTF-8"%>
             <input type='text' name='instructions' value='enter instructions' size=30></br>
             Group of the quiz:</br>
             <input type='text' name='group' value='group name' size=30></br>
-            <div id='questionDiv'>
+            Type of the quiz:
+            <select>
+                <option value='g'> graded </option>
+                <option value='ug'> ungraded </option>
+            </select> <br>
+            Start date:</br>
+            <input type='date' id='start' name='start_date' value='28-02-19' min='01-01-19' max='01-05-19'><br>
+            Start time:</br>
+            <input type='time' id='stime' name='start_time' value='13:30' min='00:00' max='23:59'><br>
+            End date:</br>
+            <input type='date' id='end' name='end_date' value='29-02-19' min='02-01-19' max='07-05-19'><br>
+            End time:</br>
+            <input type='time' id='etime' name='end_time' value='1:30' min='00:00' max='23:59'><br>
+            Duration of the quiz:</br>
+            <input type='text' id='hrs' value='0' min='0' max='3'>hrs
+            <input type='text' id='min' value='0' min='0' max='59'>mins
+
+            <div id='questionsDiv'>
                 <script>
                     var questionDict = {};
                     var nameTracingList = [];
                     var questionChoiceIterator = 0;
                 </script>
-                <input type='button' onclick='addFields(this, nameTracingList)' value='add question'>
+                <!-- <input type='button' onclick='addFields(this, nameTracingList)' value='add question'> -->
+                <input type='button' onclick='addFields(questionsDiv, nameTracingList)' value='add question'>
             </div>
             <input id="beginbtn" type="submit" value="Submit">
 
