7acfd9b2a482011503e5af96f5443318e78b3823
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-03-02 01:30:33 -0700

    Cleaned up repository of unwanted files and folders

--- Team_27/SER516_Quiz/build/javascript/quizInstructions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#startQuiz').on('click', function(){
-		var url_string = window.location.href;
-		var url = new URL(url_string);
-		var quiz = url.searchParams.get("quiz");
-		window.location.href = "StudentQuiz.html?quiz="+quiz;
-	});
-});
