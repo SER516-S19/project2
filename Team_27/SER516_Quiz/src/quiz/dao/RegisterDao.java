200503cf6ca1d4dc038cd88d2f9d41cd8dfc18f2
bpanda1 <bpanda1@asu.edu>
2019-03-02 12:28:20 -0700

    Fixed the indentation

--- Team_27/SER516_Quiz/src/quiz/dao/RegisterDao.java
+++ Team_27/SER516_Quiz/src/quiz/dao/RegisterDao.java
@@ -16,37 +16,37 @@ import quiz.Register.util.DBConnection;
  */
 
 public class RegisterDao {
-	 public String registerUser(Register registerBean)
-	 {
-	 String fullName = registerBean.getFullName();
-	 String email = registerBean.getEmail();
-	 String userName = registerBean.getUserName();
-	 String user_type = registerBean.getUserType();
-	 String password = registerBean.getPassword();
-	 Connection con = null;
-	 PreparedStatement preparedStatement = null;
-	 
-	 try
-	 {
-	 con = DBConnection.createConnection();
-	 String query = "insert into login_authentication(fullname,email,username,password,user_type) values (?,?,?,?,?)";
-	 preparedStatement = con.prepareStatement(query); //Making use of prepared statements here to insert bunch of data
-	 preparedStatement.setString(1, fullName);
-	 preparedStatement.setString(2, email);
-	 preparedStatement.setString(3, userName);
-	 preparedStatement.setString(5, user_type);
-	 preparedStatement.setString(4, password);
-	 
-	 int i= preparedStatement.executeUpdate();
-	 
-	 if (i!=0)  //Just to ensure data has been inserted into the database
-	 return "SUCCESS"; 
-	 }
-	 catch(SQLException e)
-	 {
-	 e.printStackTrace();
-	 }
-	 
-	 return "Oops.. Something went wrong there..!";  // On failure, send a message from here.
-	 }
+	public String registerUser(Register registerBean)
+	{
+		String fullName = registerBean.getFullName();
+		String email = registerBean.getEmail();
+		String userName = registerBean.getUserName();
+		String user_type = registerBean.getUserType();
+		String password = registerBean.getPassword();
+		Connection con = null;
+		PreparedStatement preparedStatement = null;
+
+		try
+		{
+			con = DBConnection.createConnection();
+			String query = "insert into login_authentication(fullname,email,username,password,user_type) values (?,?,?,?,?)";
+			preparedStatement = con.prepareStatement(query); //Making use of prepared statements here to insert bunch of data
+			preparedStatement.setString(1, fullName);
+			preparedStatement.setString(2, email);
+			preparedStatement.setString(3, userName);
+			preparedStatement.setString(5, user_type);
+			preparedStatement.setString(4, password);
+
+			int i= preparedStatement.executeUpdate();
+
+			if (i!=0)  //Just to ensure data has been inserted into the database
+				return "SUCCESS"; 
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+
+		return "Oops.. Something went wrong there..!";  // On failure, send a message from here.
+	}
 }
