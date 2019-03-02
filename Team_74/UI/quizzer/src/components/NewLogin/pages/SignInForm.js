28650c4f0cfa22dc324a93d524c3031c8a0fe05f
mspranav <smandad1@asu.edu>
2019-03-02 02:17:17 -0700

    Signin form update

--- Team_74/UI/quizzer/src/components/NewLogin/pages/SignInForm.js
+++ Team_74/UI/quizzer/src/components/NewLogin/pages/SignInForm.js
@@ -15,25 +15,38 @@ class LogInForm extends Component {
 
   render() {
     return (
-      <div>
-        <div className="FormCenter">
-              <div className="FormFields">
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                  <input type="email" id="username" className="FormField__Input" placeholder="Enter your email" name="email" ref="username" />
-                </div>
-
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="password">Password</label>
-                  <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" ref="password" />
-                </div>
-
-                <div className="FormField">
-                  <button className="FormField__Button mr-20" onClick={this.handleSignIn.bind(this)} >Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
-                </div>
-              </div>
-            </div>
-      </div>
+        <form className="FormCenter" onSubmit={this.handleSignIn}>
+          <div className={'FormField' + (submitted && !username ? ' has-error' : '')}>
+            <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+            <input type="email" className="FormField__Input" placeholder="Enter your email" name="username" value={username} onChange={this.handleChange} />
+            {submitted && !username &&
+            <div className="help-block">Email is required</div>
+            }
+          </div>
+          <div className={'FormField' + (submitted && !password ? ' has-error' : '')}>
+            <label className="FormField__Label" htmlFor="password">Password</label>
+            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={password} onChange={this.handleChange} />
+            {submitted && !password &&
+            <div className="help-block">Password is required</div>
+            }
+          </div>
+          <div className="FormField">
+            <button className="FormField__Button mr-20" disabled={loading}>Login</button>
+            {loading &&
+            <img src="data:images/loading.gif;base64,
+            R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/
+            C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh
+            +QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIj
+            K8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUk
+            GCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/
+            jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+            }
+            <Link to="/" className="FormField__Link">Create an account</Link>
+          </div>
+          {error &&
+          <div className={'alert alert-danger'}>{error}</div>
+          }
+        </form>
     );
   }
 }
