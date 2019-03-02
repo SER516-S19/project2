8dc1f83b93f873e06b249893bc20a73e40768deb
Harshita Kajal <37720802+HarshitaKAM@users.noreply.github.com>
2019-03-01 17:22:36 -0700

    Removing extra spaces to maintain coding standards

--- Team_74/Back-End/Quizzer/src/main/java/com/Quizzer/code/dao/SubmitQuizRepo.java
+++ Team_74/Back-End/Quizzer/src/main/java/com/Quizzer/code/dao/SubmitQuizRepo.java
@@ -7,12 +7,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.Quizzer.code.model.db.Quiz;
 import com.Quizzer.code.model.db.SubmitQuiz;
 
-
 /**
  * This repository implements the mongo repository for StudentQuiz table.
- * 
  * @author Kirti Jha
- *
  */
 public interface SubmitQuizRepo extends MongoRepository<SubmitQuiz, String> {
 
