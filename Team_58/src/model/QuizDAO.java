dfd96041e2ca9ff17fc7b631d066e2b35e3946fb
Carnic <ca108@snu.edu.in>
2019-03-14 22:15:52 -0700

    code refactoring - QuizDAO

--- Team_58/src/model/QuizDAO.java
+++ Team_58/src/model/QuizDAO.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 /**
  * Interface QuizDAO with the following method:
- * 	a. get quizzes under this Course
+ *  a. get quizzes under this Course
  *  b. enter quiz details
  *  c. get quiz Id
  * 
- * @author narenkumarkonchada & carnic
- * @version 1.0
- * @date 02/21/2019
+ * @author narenkumarkonchada 
+ * @author carnic
+ * @version 1.1
+ * @date 03/14/2019
  * 
  */
 public interface QuizDAO {
+	
 	public List<QuizVO> getQuizzesForCourse(int courseId) throws SQLException, ClassNotFoundException;
 	public void insertingQuizDetails (QuizVO quizVO) throws  SQLException, ClassNotFoundException;
 	public int gettingQuizId (QuizVO quizVO) throws  SQLException, ClassNotFoundException;
