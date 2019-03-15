62fb93592b479f2b661a2c54ff13f11966b30d74
Dhruv Patel <dhruvpatel0127@gmail.com>
2019-03-14 20:36:07 -0700

    Cleaned code

--- Team_58/src/controller/DisplayGradesServlet.java
+++ Team_58/src/controller/DisplayGradesServlet.java
@@ -1,27 +1,28 @@
 package controller;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.annotation.WebServlet;
+
 import model.GradeQuizVO;
 import model.GradeQuizVODAO;
 import model.GradeQuizVODAOBean;
 import model.UserVO;
-
-import java.sql.PreparedStatement;
-import javax.servlet.annotation.WebServlet;
-
-import java.util.List;
-import java.util.logging.Logger;
 /**
  * Display grades of the quiz
  * @author Dhruv Patel
- * @version 1.1
- * @date 03/1/2019
+ * @version 1.2
+ * @date 03/14/2019
  **/
 
 @WebServlet(name = "DisplayGradesServlet", urlPatterns = "/displayGrades")
@@ -39,13 +40,10 @@ public class DisplayGradesServlet extends HttpServlet {
         try {
         	HttpSession session = req.getSession();
     		UserVO userVO = (UserVO) session.getAttribute("userVO");
-			
     		GradeQuizVODAOBean gradeQuizVODAOBean = new GradeQuizVODAOBean();
-			List<GradeQuizVO> grades = gradeQuizVODAOBean.getQuizGradesForStudent(userVO.getUserId());
+		List<GradeQuizVO> grades = gradeQuizVODAOBean.getQuizGradesForStudent(userVO.getUserId());
     		session.setAttribute("Grades", grades);
-			
 			res.sendRedirect(req.getContextPath() + "/displayGrades.ftl");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
