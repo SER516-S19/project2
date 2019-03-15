3c39c647050b52eec00b67d068ca8d2db73e8d12
Carnic <ca108@snu.edu.in>
2019-03-14 22:24:53 -0700

    code refactoring - UserDAOBean

--- Team_58/src/model/UserDAOBean.java
+++ Team_58/src/model/UserDAOBean.java
@@ -9,17 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-
 /**
  * This class implements UserDAO interface, establishes database connection 
  * using ConnectionFactory and fetches user information.
  * 
  * @author Aditya Vikram
- * @version 1.1
- * @date 02/22/2019
+ * @version 1.2
+ * @date 03/14/2019
  */
-
-
 public class UserDAOBean implements UserDAO {
 
 	public static Properties dbProperties = new Properties();
@@ -47,20 +44,18 @@ public class UserDAOBean implements UserDAO {
 	@Override
 	public List<UserVO> getUserInfo(String userName, String passWord) throws ClassNotFoundException, SQLException, IOException {
 		
-		List<UserVO> list = new ArrayList<UserVO>();
-		
-        Connection connection = ConnectionFactory.getConnection();
-        ResultSet resultData = null;
-        PreparedStatement query = null;
+		List<UserVO> list = new ArrayList<UserVO>();	
+        	Connection connection = ConnectionFactory.getConnection();
+	        ResultSet resultData = null;
+        	PreparedStatement query = null;
         
-        System.out.println("validateAndGet"+ connection);
-        System.out.println(dbProperties.getProperty("getUserDetailQuery"));
+	        System.out.println("validateAndGet"+ connection);
+        	System.out.println(dbProperties.getProperty("getUserDetailQuery"));
         
 		try{             
 			query=connection.prepareStatement(dbProperties.getProperty("getUserDetailQuery"));  
 			query.setString(1,userName);  
 			query.setString(2,passWord);  
-			 
 			resultData = query.executeQuery();
 		
 			while(resultData.next()) {
@@ -87,14 +82,13 @@ public class UserDAOBean implements UserDAO {
 
 	@Override
 	public void updatePassword(String userName, String password)
-			throws ClassNotFoundException, SQLException, IOException
-	{
-        Connection connection = ConnectionFactory.getConnection();
-        PreparedStatement query = null;
+			throws ClassNotFoundException, SQLException, IOException{
+		
+        	Connection connection = ConnectionFactory.getConnection();
+	        PreparedStatement query = null;
 		query=connection.prepareStatement(dbProperties.getProperty("updatePassword"));
 		query.setString(2,userName);
 		query.setString(1,password);
 		query.executeUpdate();
 	}
-
 }
