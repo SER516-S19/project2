6b4f70e1a973dad24e735e6fed18df91fff562d7
Carnic <ca108@snu.edu.in>
2019-03-14 22:27:13 -0700

    code refactoring - ViewQuizDAO

--- Team_58/src/model/ViewQuizDAO.java
+++ Team_58/src/model/ViewQuizDAO.java
@@ -2,22 +2,18 @@ package model;
 
 import java.sql.SQLException;
 import java.util.List;
-
 import org.json.simple.parser.ParseException;
 
 /**
  * Interface
  * 
  * @author Aditya Samant
- * @version 1.0
- * @date 02/22/2019
- * 
+ * @version 1.1
+ * @date 03/14/2019
  */
-
 public interface ViewQuizDAO {
+	
 	public QuizVO getQuizInfo(int quizId) throws SQLException, ClassNotFoundException;
 	public List<displayQuestionsVO> getStudentQuestionsInfo(int quizId) throws SQLException, ClassNotFoundException, ParseException;
-
-	public List<QuestionVO> getQuestionsInfo(int quizId) throws SQLException, ClassNotFoundException, ParseException;
-		
+	public List<QuestionVO> getQuestionsInfo(int quizId) throws SQLException, ClassNotFoundException, ParseException;	
 }
